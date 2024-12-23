42016722aa2c63b6d6cda63179c4d891468fb8e6
khpolash <khpolashbd@gmail.com>
2024-12-23 10:55:03 +0600

    Added new entity relationships and DbSets for Project-related entities
    - Updated `OnModelCreating` to define new relationships between Project and its related entities (e.g., ReceivePayments, SubProjects, VatTaxes, BGBFunds, SignatoryAuthorities).
    - Added DbSets for new entities such as ProjectType, VatTax, BGBFund, ContractorPayment, and ContractorBillPayment.
    - Ensured cascading delete behavior is disabled for the related entities.

--- BGB.Data/Database/IDIMDBEntities.cs
+++ BGB.Data/Database/IDIMDBEntities.cs
@@ -1,6 +1,5 @@
 ﻿using BGB.Data.Entities.Admin;
 using BGB.Data.Entities.Budget;
-using BGB.Data.Entities.Central;
 using BGB.Data.Entities.Irms;
 using BGB.Data.Entities.Pm;
 using BGB.Data.SqlViews.Pm;
@@ -18,6 +17,48 @@ namespace BGB.Data.Database
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+
+            modelBuilder.Entity<Project>()
+                .HasMany(e => e.ReceivePayments)
+                .WithRequired(e => e.Project)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Project>()
+                .HasMany(e => e.SubProjects)
+                .WithRequired(e => e.Project)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Project>()
+                .HasMany(e => e.VatTaxes)
+                .WithRequired(e => e.Project)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Project>()
+                .HasMany(e => e.BGBFunds)
+                .WithRequired(e => e.Project)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Project>()
+                .HasMany(e => e.SignatoryAuthorities)
+                .WithRequired(e => e.Project)
+                .WillCascadeOnDelete(false);
+
+
+            modelBuilder.Entity<SubProject>()
+                .HasMany(e => e.BillPayments)
+                .WithRequired(e => e.SubProject)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<SubProject>()
+              .HasMany(e => e.ProjectStatus)
+              .WithRequired(e => e.SubProject)
+              .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<SubProject>()
+              .HasMany(e => e.ContractorPayments)
+              .WithRequired(e => e.SubProject)
+              .WillCascadeOnDelete(false);
+
 
             modelBuilder.Entity<Entities.Admin.Application>()
                 .HasMany(x => x.ActivityLogs)
@@ -56,6 +97,13 @@ namespace BGB.Data.Database
         public virtual DbSet<Role> Roles { get; set; }
         #endregion
 
+        public virtual DbSet<ProjectType> ProjectTypes { get; set; }
+        public virtual DbSet<SignatoryAuthority> SignatoryAuthorities { get; set; }
+        public virtual DbSet<VatTax> VatTaxes { get; set; }
+        public virtual DbSet<BGBFund> BGBFunds { get; set; }
+        public virtual DbSet<ContractorPayment> ContractorPayments { get; set; }
+        public virtual DbSet<ContractorBillPayment> ContractorBillPayments { get; set; }
+
         public virtual DbSet<IrmsSetupLocation> SetupLocations { get; set; }
         public virtual DbSet<Attachment> Attachments { get; set; }
         public virtual DbSet<BillPayment> BillPayments { get; set; }
@@ -64,8 +112,8 @@ namespace BGB.Data.Database
         public virtual DbSet<ProjectExtend> ProjectExtends { get; set; }
         public virtual DbSet<ProjectProblem> ProjectProblems { get; set; }
         public virtual DbSet<ProjectStatus> ProjectStatus { get; set; }
-        public virtual DbSet<PmQuarter> Quarters { get; set; }
-        public virtual DbSet<ReceiptPayment> ReceiptPayments { get; set; }
+        public virtual DbSet<Quarter> Quarters { get; set; }
+        public virtual DbSet<ReceivePayment> ReceivePayments { get; set; }
         public virtual DbSet<ViewProjectInformationRpt> ViewProjectInformationRpts { get; set; }
         public virtual DbSet<ViewProjectPaymentReceiptRpt> ViewProjectPaymentReceiptRpts { get; set; }
         public virtual DbSet<ViewProjectProblemRpt> ViewProjectProblemRpts { get; set; }
@@ -86,7 +134,7 @@ namespace BGB.Data.Database
         public virtual DbSet<FundRelease> FundReleases { get; set; }
         public virtual DbSet<ViewExtended> ViewExtendeds { get; set; }
         public virtual DbSet<ViewProjectProblem> ViewProjectProblems { get; set; }
-        public virtual DbSet<PmSecurityDeposit> SecurityDeposits { get; set; }
+        public virtual DbSet<SecurityDeposit> SecurityDeposits { get; set; }
     }
 }
 
