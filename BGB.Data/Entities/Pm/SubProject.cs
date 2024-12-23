5925247e9fc0a2e84eed3460c75043c862957b09
khpolash <khpolashbd@gmail.com>
2024-12-23 11:16:52 +0600

    Update SubProject Entity: Enhance SubProject Structure and Navigation Properties
    - Updated `SubProject` entity for `pm.SubProject` table with additional properties:
      - `BankGuarantee`, `BankGuaranteeEndDate`, `NOA`, `NOADate`, `Agreement`, `AgreementDate`, `WorkOrder`, `WorkOrderDate`
    - Changed `AgreementCost` type to `decimal` for precision.
    - Updated the `Description` field to be non-nullable.
    - Replaced `SecurityDeposit1` and `ConsultancyActivities` with `ContractorPayments`.
    - Enhanced navigation properties: added `ContractorPayments` and `BillPayments` collections.
    - Established foreign key relationships for `Project` and `ConstructionFirm`.

--- BGB.Data/Entities/Pm/SubProject.cs
+++ BGB.Data/Entities/Pm/SubProject.cs
@@ -1,4 +1,3 @@
-using BGB.Data.Entities.Irms;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -12,70 +11,48 @@ namespace BGB.Data.Entities.Pm
         public SubProject()
         {
             BillPayments = new HashSet<BillPayment>();
-            ConsultancyActivities = new HashSet<ConsultancyActivity>();
-            ConsultancyFees = new HashSet<ConsultancyFee>();
-            ProjectExtends = new HashSet<ProjectExtend>();
-            ProjectProblems = new HashSet<ProjectProblem>();
             ProjectStatus = new HashSet<ProjectStatus>();
-            SecurityDeposit1 = new HashSet<PmSecurityDeposit>();
-            SubProjectDetails = new HashSet<SubProjectDetail>();
+            ContractorPayments = new HashSet<ContractorPayment>();
         }
+
         [Key]
         public int SubProjectId { get; set; }
-
         public int ProjectId { get; set; }
 
         [StringLength(255)]
         public string SubProjectTitle { get; set; }
-
-        public int UnitId { get; set; }
-
         public string Description { get; set; }
-
         public int ConstructionFirmId { get; set; }
-
-        public double AgreementCost { get; set; }
-
-        public DateTime? StartDate { get; set; }
-
-        public DateTime? EndDate { get; set; }
-
-        public int Status { get; set; }
-
-        public DateTime? HandOverDate { get; set; }
-
+        public decimal AgreementCost { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime HandOverDate { get; set; }
+        public string BankGuarantee { get; set; }
+        public DateTime? BankGuaranteeEndDate { get; set; }
+        public string NOA { get; set; }
+        public DateTime? NOADate { get; set; }
+        public string Agreement { get; set; }
+        public DateTime? AgreementDate { get; set; }
+        public string WorkOrder { get; set; }
+        public DateTime? WorkOrderDate { get; set; }
         public string Remark { get; set; }
-
         public int CreatedUser { get; set; }
-
         public DateTime CreatedDateTime { get; set; }
-
         public int? UpdatedUser { get; set; }
-
         public DateTime? UpdatedDateTime { get; set; }
-
         public int UpdateNo { get; set; }
 
-        public virtual IrmsSetupUnit IrmsSetupUnit { get; set; }
-
-        public virtual ICollection<BillPayment> BillPayments { get; set; }
-
-        public virtual ConstructionFirm ConstructionFirm { get; set; }
-
-        public virtual ICollection<ConsultancyActivity> ConsultancyActivities { get; set; }
-
-        public virtual ICollection<ConsultancyFee> ConsultancyFees { get; set; }
-
+        [ForeignKey(nameof(ProjectId))]
         public virtual Project Project { get; set; }
 
-        public virtual ICollection<ProjectExtend> ProjectExtends { get; set; }
+        [ForeignKey(nameof(ConstructionFirmId))]
+        public virtual ConstructionFirm ConstructionFirm { get; set; }
 
-        public virtual ICollection<ProjectProblem> ProjectProblems { get; set; }
-
+        #region Navigation Properties
+        public virtual ICollection<BillPayment> BillPayments { get; set; }
         public virtual ICollection<ProjectStatus> ProjectStatus { get; set; }
+        public virtual ICollection<ContractorPayment> ContractorPayments { get; set; }
 
-        public virtual ICollection<PmSecurityDeposit> SecurityDeposit1 { get; set; }
-
-        public virtual ICollection<SubProjectDetail> SubProjectDetails { get; set; }
+        #endregion
     }
 }
