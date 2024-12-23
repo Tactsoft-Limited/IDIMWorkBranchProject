b3cd4c4b50bca2dbeff8a76789f0bda30fb130bf
khpolash <khpolashbd@gmail.com>
2024-12-23 11:18:43 +0600

    Refactor Dependency Injection Configuration with Autofac
    - Simplified the DependencyConfig class for Autofac-based DI setup.
    - Registered AutoMapper configuration and mapper instance as a singleton.
    - Registered controllers and services dynamically using reflection for better maintainability.
    - Cleaned up commented-out code for unused services to improve readability.
    - Configured filter provider and view registration for custom views.
    - Removed redundant registration of services and streamlined the process.

--- IDIMWorkBranchProject/App_Start/DependencyConfig.cs
+++ IDIMWorkBranchProject/App_Start/DependencyConfig.cs
@@ -2,11 +2,7 @@
 using System.Web.Mvc;
 using Autofac;
 using AutoMapper;
-using IDIMWorkBranchProject.Services.Setup;
-using IDIMWorkBranchProject.Services.User;
 using Autofac.Integration.Mvc;
-using IDIMWorkBranchProject.Services.Dashboard;
-using IDIMWorkBranchProject.Services.WBP;
 using BGB.Data.Database;
 using System.Reflection;
 
@@ -54,45 +50,6 @@ namespace IDIMWorkBranchProject
             var dataAccess = Assembly.GetExecutingAssembly();
             builder.RegisterAssemblyTypes(dataAccess).Where(t => t.Name.EndsWith("Service")).AsImplementedInterfaces();
 
-            //// Register dependencies in filter attributes
-            //builder.RegisterFilterProvider();
-
-            //// Register dependencies in custom views
-            //builder.RegisterSource(new ViewRegistrationSource());
-
-            //#region wbp
-            //builder.Register(c => new DashboardService(c.ResolveNamed<IMapper>("IDIM"))).As<IDashboardService>().InstancePerLifetimeScope();
-            //builder.Register(c => new ProjectService(c.ResolveNamed<IMapper>("IDIM"))).As<IProjectService>().InstancePerLifetimeScope();
-            //builder.Register(c => new SubProjectService(c.ResolveNamed<IMapper>("IDIM"))).As<ISubProjectService>().InstancePerLifetimeScope();
-            //builder.Register(c => new BillPaymentService(c.ResolveNamed<IMapper>("IDIM"))).As<IBillPaymentService>().InstancePerLifetimeScope();
-            //builder.Register(c => new ProjectExtendService(c.ResolveNamed<IMapper>("IDIM"))).As<IProjectExtendService>().InstancePerLifetimeScope();
-            //builder.Register(c => new ProjectProblemService(c.ResolveNamed<IMapper>("IDIM"))).As<IProjectProblemService>().InstancePerLifetimeScope();
-            //builder.Register(c => new ProjectStatusService(c.ResolveNamed<IMapper>("IDIM"))).As<IProjectStatusService>().InstancePerLifetimeScope();
-            //builder.Register(c => new ReceiptPaymentService(c.ResolveNamed<IMapper>("IDIM"))).As<IReceiptPaymentService>().InstancePerLifetimeScope();
-            //builder.Register(c => new ConsultantService(c.ResolveNamed<IMapper>("IDIM"))).As<IConsultantService>().InstancePerLifetimeScope();
-            //builder.Register(c => new SubProjectDetailsService(c.ResolveNamed<IMapper>("IDIM"))).As<ISubProjectDetailsService>().InstancePerLifetimeScope();
-            //builder.Register(c => new ConsultancyFeesService(c.ResolveNamed<IMapper>("IDIM"))).As<IConsultancyFeesService>().InstancePerLifetimeScope();
-            //builder.Register(c => new ConsultancyActivitiesService(c.ResolveNamed<IMapper>("IDIM"))).As<IConsultancyActivitiesService>().InstancePerLifetimeScope();
-            //builder.Register(c => new SecurityDepositService(c.ResolveNamed<IMapper>("IDIM"))).As<ISecurityDepositService>().InstancePerLifetimeScope();
-            //#endregion
-
-            //#region setup
-            //builder.Register(c => new FiscalYearService(c.ResolveNamed<IMapper>("IDIM"))).As<IFiscalYearService>().InstancePerLifetimeScope();
-            //builder.Register(c => new BillTypeService(c.ResolveNamed<IMapper>("IDIM"))).As<IBillTypeService>().InstancePerLifetimeScope();
-            //builder.Register(c => new QuarterService(c.ResolveNamed<IMapper>("IDIM"))).As<IQuarterService>().InstancePerLifetimeScope();
-            //builder.Register(c => new ConstructionFirmService(c.ResolveNamed<IMapper>("IDIM"))).As<IConstructionFirmService>().InstancePerLifetimeScope();
-            //builder.Register(c => new UnitService(c.ResolveNamed<IMapper>("IDIM"))).As<IUnitService>().InstancePerLifetimeScope();
-            //#endregion
-
-            //#region security
-            //builder.Register(c => new ApplicationService(c.ResolveNamed<IMapper>("IDIM"))).As<IApplicationService>().InstancePerLifetimeScope();
-            //builder.Register(c => new DeviceService(c.ResolveNamed<IMapper>("IDIM"))).As<IDeviceService>().InstancePerLifetimeScope();
-            //builder.Register(c => new GeneralInformationService(c.ResolveNamed<IMapper>("IDIM"))).As<IGeneralInformationService>().InstancePerLifetimeScope();
-            //builder.Register(c => new MenuService(c.ResolveNamed<IMapper>("IDIM"))).As<IMenuService>().InstancePerLifetimeScope();
-            //builder.Register(c => new UserService(c.ResolveNamed<IMapper>("IDIM"))).As<IUserService>().InstancePerLifetimeScope();
-            //builder.Register(c => new UserPriviledgeService(c.ResolveNamed<IMapper>("IDIM"))).As<IUserPriviledgeService>().InstancePerLifetimeScope();
-            //builder.Register(c => new UserApplicationService(c.ResolveNamed<IMapper>("IDIM"))).As<IUserApplicationService>().InstancePerLifetimeScope();
-            //#endregion
         }
     }
 }
