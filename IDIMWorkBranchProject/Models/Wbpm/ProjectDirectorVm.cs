850b38e57fb7aaf5a9b3289ec38ea8099702b50f
khpolash <khpolashbd@gmail.com>
2025-01-13 13:06:54 +0600

    Add: ProjectDirectorVm class to manage project director details
    - Introduced `ProjectDirectorVm` class to store and manage information about project directors.
    - Includes properties such as `ADPProjectId`, `ProjectTitle`, `ProjectDirectorName`, `Designation`, and document handling (`PDDocument`, `PDDocumentFile`).
    - Supports date fields for `JoiningDate` and `TransferDate`, with custom date formatting.
    - Implements validation via `ProjectDirectorVmValidator` for ensuring proper data integrity.
    - Allows for uploading and attaching project director documents via `PDDocumentFile`.
    - Tracks the status of the project director with the `IsActive` flag.

---
+++ IDIMWorkBranchProject/Models/Wbpm/ProjectDirectorVm.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using System;
+using System.Web;
+using FluentValidation.Attributes;
+using IDIMWorkBranchProject.Models.Validation.Wbpm;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace IDIMWorkBranchProject.Models.Wbpm
+{
+    [Validator(typeof(ProjectDirectorVmValidator))]
+    public class ProjectDirectorVm
+    {
+
+        public int ProjectDirectorId { get; set; }
+
+        [DisplayName("প্রকল্পের নাম")]
+        public int ADPProjectId { get; set; }
+
+        [DisplayName("প্রকল্পের শিরোনাম")]
+        public string ProjectTitle { get; set; }
+
+        [DisplayName("প্রকল্প পরিচালক")]
+        public string ProjectDirectorName { get; set; }
+
+        [DisplayName("পদবী")]
+        public string Designation { get; set; }
+
+        [DisplayName("যোগদান তারিখ")]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? JoiningDate { get; set; }
+
+        [DisplayName("বদলীর তারিখ")]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? TransferDate { get; set; }
+
+        [DisplayName("ডকুমেন্ট")]
+        public string PDDocument { get; set; }
+
+        public bool IsActive { get; set; }
+
+        [NotMapped]
+        [DisplayName("আপলোড ডকুমেন্ট")]
+        public HttpPostedFileBase PDDocumentFile { get; set; }
+
+
+    }
+}
