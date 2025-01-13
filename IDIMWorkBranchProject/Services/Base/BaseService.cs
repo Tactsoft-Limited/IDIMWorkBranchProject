1b57526e2e04679eeaa2276c5c45aed08e0d0995
khpolash <khpolashbd@gmail.com>
2025-01-13 13:07:54 +0600

    Add: BaseService<TEntity> class for generic CRUD operations
    - Introduced the `BaseService<TEntity>` class to provide generic CRUD functionality for entities.
    - Includes methods for creating (`CreateAsync`), reading (`GetAllAsync`, `GetByIdAsync`), updating (`UpdateAsync`), and deleting (`DeleteAsync`) entities.
    - Uses `IDIMDBEntities` as the data context and provides access to the `DbSet<TEntity>` to perform operations on the corresponding table in the database.
    - Supports asynchronous operations to handle database interactions efficiently.
    - The class allows extension for more specific services by overriding methods like `CreateAsync` and `UpdateAsync` if needed.
    - Facilitates easy management of entity data across the application.

---
+++ IDIMWorkBranchProject/Services/Base/BaseService.cs
@@ -0,0 +1,66 @@
+﻿using IDIMWorkBranchProject.Data.Database;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IDIMWorkBranchProject.Services.Base
+{
+    public class BaseService<TEntity> : IBaseService<TEntity> where TEntity : class
+    {
+        protected readonly IDIMDBEntities _context;
+        protected readonly DbSet<TEntity> _dbSet;
+
+        public BaseService(IDIMDBEntities context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public IQueryable<TEntity> GetQueryableEntities()
+        {
+            return _dbSet.AsQueryable();
+        }
+
+        // Create
+        public virtual async Task<TEntity> CreateAsync(TEntity entity)
+        {
+            _dbSet.Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(object id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        // Update
+        public virtual async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<TEntity> DeleteAsync(object id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+            if (entity == null)
+            {
+                return null;
+            }
+
+            _dbSet.Remove(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+    }
+
+}
