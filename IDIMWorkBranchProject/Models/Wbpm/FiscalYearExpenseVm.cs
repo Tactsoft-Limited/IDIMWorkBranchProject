eadeb7355f45e29334e7e6902c23ec45dd65804e
khpolash <khpolashbd@gmail.com>
2025-01-13 13:04:21 +0600

    Add: FiscalYearExpenseVm class for managing fiscal year expenses in ADP projects
    - Introduced `FiscalYearExpenseVm` class to track the expenses for ADP projects across different fiscal years.
    - Contains fields for the project ID, project title, fiscal year, and total expenses for the fiscal year.
    - The class provides a way to manage and validate the expenses related to a project for each fiscal year.
    - Implements data validation via FluentValidation using `FiscalYearExpenseVmValidator`.
    - Ensures that the expenses are properly allocated and tracked for reporting and project financial management.

---
+++ IDIMWorkBranchProject/Models/Wbpm/FiscalYearExpenseVm.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using FluentValidation.Attributes;
+using IDIMWorkBranchProject.Models.Validation.Wbpm;
+
+namespace IDIMWorkBranchProject.Models.Wbpm
+{
+    [Validator(typeof(FiscalYearExpenseVmValidator))]
+    public class FiscalYearExpenseVm
+    {
+
+
+        public int FiscalYearExpenseId { get; set; }
+
+        [DisplayName("প্রকল্প আইডি")]
+        public int ADPProjectId { get; set; }
+
+        [DisplayName("প্রকল্পের শিরোনাম")]
+        public string ProjectTitle { get; set; }
+
+        [DisplayName("আর্থবছর")]
+        public int FiscalYearId { get; set; }
+
+        [DisplayName("আর্থবছর")]
+        public string FiscalYearDescription { get; set; }
+
+        [DisplayName("মোট খরচ")]
+        public float TotalExpense { get; set; }
+
+    }
+}
