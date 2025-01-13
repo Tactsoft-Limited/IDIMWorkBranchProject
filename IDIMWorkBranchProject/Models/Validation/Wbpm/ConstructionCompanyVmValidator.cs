845ccc6630ca3c97ead06805a82a4d8e33e508ab
khpolash <khpolashbd@gmail.com>
2025-01-13 12:50:23 +0600

    Add: Validator for ConstructionCompanyVm to enforce business rules
    - Created `ConstructionCompanyVmValidator` using FluentValidation to validate the properties of the `ConstructionCompanyVm` model.
    - Added validation rules for FirmName, FirmNameB, ContactPerson, ContactPersonB, ContactPhone, Email, FirmAddress, and FirmAddressB.
    - Error messages are provided in Bengali for user-friendly validation feedback, ensuring proper input for firm details.

---
+++ IDIMWorkBranchProject/Models/Validation/Wbpm/ConstructionCompanyVmValidator.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using IDIMWorkBranchProject.Models.Wbpm;
+
+namespace IDIMWorkBranchProject.Models.Validation.Wbpm
+{
+    public class ConstructionCompanyVmValidator : AbstractValidator<ConstructionCompanyVm>
+    {
+        public ConstructionCompanyVmValidator()
+        {
+            RuleFor(x => x.FirmName)
+                .NotEmpty().WithMessage("ফার্মের নাম প্রদান করুন")
+                .MaximumLength(200).WithMessage("ফার্মের নাম সর্বাধিক 200 অক্ষর হতে হবে");
+
+            RuleFor(x => x.FirmNameB)
+                .NotEmpty().WithMessage("ফার্মের নাম (বাংলা) প্রদান করুন")
+                .MaximumLength(200).WithMessage("ফার্মের নাম (বাংলা) সর্বাধিক 200 অক্ষর হতে হবে");
+
+            RuleFor(x => x.ContactPerson)
+                .NotEmpty().WithMessage("যোগাযোগ ব্যক্তির নাম প্রদান করুন")
+                .MaximumLength(100).WithMessage("যোগাযোগ ব্যক্তির নাম সর্বাধিক 100 অক্ষর হতে হবে");
+
+            RuleFor(x => x.ContactPersonB)
+               .NotEmpty().WithMessage("যোগাযোগ ব্যক্তির নাম (বাংলা) প্রদান করুন")
+               .MaximumLength(150).WithMessage("যোগাযোগ ব্যক্তির নাম (বাংলা) সর্বাধিক 150 অক্ষর হতে হবে");
+
+            RuleFor(x => x.ContactPhone)
+                .NotEmpty().WithMessage("ফোন নম্বর প্রদান করুন")
+                .MaximumLength(50).WithMessage("ফোন নম্বর সর্বাধিক 50 অক্ষর হতে হবে");
+
+            RuleFor(x => x.Email)
+                .EmailAddress().WithMessage("ভুল ইমেইল ঠিকানা প্রদান করা হয়েছে");
+
+            RuleFor(x => x.FirmAddress)
+                .NotEmpty().WithMessage("ফার্মের ঠিকানা প্রদান করুন")
+                .MaximumLength(500).WithMessage("ফার্মের ঠিকানা সর্বাধিক 500 অক্ষর হতে হবে");
+
+            RuleFor(x => x.FirmAddressB)
+                .NotEmpty().WithMessage("ফার্মের ঠিকানা (বাংলা) প্রদান করুন")
+                .MaximumLength(500).WithMessage("ফার্মের ঠিকানা (বাংলা) সর্বাধিক 500 অক্ষর হতে হবে");
+        }
+    }
+}
