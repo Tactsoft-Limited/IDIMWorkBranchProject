9689f09caa7b5eebb085d5d0a5e7e5d224fda1e7
khpolash <khpolashbd@gmail.com>
2025-01-13 12:43:34 +0600

    Add: Define FinancialYearAllocation entity with financial allocation properties
    - Created FinancialYearAllocation class with properties for total, ADP, and RADP allocations.
    - Added foreign key relationships to the ADPProject and FiscalYear entities.
    - Inherited from BaseEntity to include created/updated timestamps and user information.

---
+++ BGB.Data/Entities/Wbpm/FinancialYearAllocation.cs
@@ -0,0 +1,33 @@
+﻿using BGB.Data.Entities.Budget;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+using BGB.Data.Entities.Base;
+
+namespace BGB.Data.Entities.Wbpm
+{
+
+    [Table("FinancialYearAllocation", Schema = "wbpm")]
+    public class FinancialYearAllocation : BaseEntity
+    {
+
+
+        [Key]
+        public int FinancialYearAllocationId { get; set; }
+
+        public int ADPProjectId { get; set; }
+        public int FiscalYearId { get; set; }
+        public float TotalAllocation { get; set; }
+        public float ADPAllocation { get; set; }
+        public float RADPAllocation { get; set; }
+
+
+        #region Navigation Property
+        [ForeignKey(nameof(ADPProjectId))]
+        public virtual ADPProject ADPProject { get; set; }
+
+        [ForeignKey(nameof(FiscalYearId))]
+        public virtual FiscalYear FiscalYear { get; set; }
+        #endregion
+    }
+}
