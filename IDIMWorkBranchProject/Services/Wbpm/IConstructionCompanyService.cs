2c6e25cb33425c083dc0ba67c623fc5bd6314be0
khpolash <khpolashbd@gmail.com>
2025-01-13 12:35:42 +0600

    Add: Define IBGBMiscellaneousFundService interface
    - Created IBGBMiscellaneousFundService interface to extend IBaseService for BGBMiscellaneousFund entity.
    - Ensured consistency with base service structure for managing BGBMiscellaneousFund operations.

---
+++ IDIMWorkBranchProject/Services/Wbpm/IConstructionCompanyService.cs
@@ -0,0 +1,12 @@
+﻿using BGB.Data.Entities.Wbpm;
+using IDIMWorkBranchProject.Models.Wbpm;
+using IDIMWorkBranchProject.Services.Base;
+using System.Threading.Tasks;
+
+namespace IDIMWorkBranchProject.Services.Wbpm
+{
+    public interface IConstructionCompanyService : IBaseService<ConstructionCompany>
+    {
+        Task<object> GetPagedAsync(ConstructionCompanySearchVm model);
+    }
+}
