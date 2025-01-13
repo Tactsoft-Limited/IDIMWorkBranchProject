5fd9eb7c17403aeca34ce0e3154c4b76238541ea
khpolash <khpolashbd@gmail.com>
2025-01-13 12:39:59 +0600

    Add: BaseEntity class to handle common entity properties
    - Created BaseEntity class with properties for tracking created and updated user information, timestamps, and update number.

---
+++ BGB.Data/Entities/Base/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BGB.Data.Entities.Base
+{
+    public class BaseEntity
+    {
+        public int? CreatedUser { get; set; }
+        public DateTime? CreatedDateTime { get; set; }
+        public int? UpdatedUser { get; set; }
+        public DateTime? UpdatedDateTime { get; set; }
+        public int? UpdateNo { get; set; }
+    }
+
+}
