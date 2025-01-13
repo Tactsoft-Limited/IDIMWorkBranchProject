67f7e8c05bc34979975db6a7757828b8dd25b761
khpolash <khpolashbd@gmail.com>
2025-01-13 12:42:48 +0600

    Add: Define ConstructionCompany entity with properties and navigation
    - Created ConstructionCompany class with properties such as FirmName, ContactPerson, ContactPhone, Email, and Address.
    - Added navigation property to link ProjectWork entities.
    - Inherited from BaseEntity to include created/updated timestamps and user information.

---
+++ BGB.Data/Entities/Wbpm/ConstructionCompany.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+using BGB.Data.Entities.Base;
+
+namespace BGB.Data.Entities.Wbpm
+{
+    [Table("ConstructionCompany", Schema = "wbpm")]
+    public class ConstructionCompany : BaseEntity
+    {
+
+
+        [Key]
+        public int ConstructionCompanyId { get; set; }
+
+        public string FirmName { get; set; }
+        public string FirmNameB { get; set; }
+        public string ContactPerson { get; set; }
+        public string ContactPersonB { get; set; }
+        public string ContactPhone { get; set; }
+        public string Email { get; set; }
+        public string FirmAddress { get; set; }
+        public string FirmAddressB { get; set; }
+
+
+        #region  Navigation Property
+        public virtual ICollection<ProjectWork> ProjectWorks { get; set; }
+        #endregion
+    }
+}
