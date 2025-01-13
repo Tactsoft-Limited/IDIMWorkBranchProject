07c4da83fe126fd211102567aea13864ca25e08c
khpolash <khpolashbd@gmail.com>
2025-01-13 12:45:21 +0600

    Add: Define ProjectWork entity for ADPProject management
    - Created ProjectWork class to represent work details for each ADPProject.
    - Added fields for project work title, estimated cost, agreement, work order, start/end dates, and bank guarantee details.
    - Included navigation properties to relate ProjectWork to ADPProject and ConstructionCompany entities.
    - Introduced collections for related entities such as ContractorCompanyPayment, ADPReceivePayment, and BGBMiscellaneousFund.
    - Inherited from BaseEntity to include standard audit fields (created/updated timestamps and user information).
    - Added fields for construction progress, work status, and relevant documents.

---
+++ BGB.Data/Entities/Wbpm/ProjectWork.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+using BGB.Data.Entities.Base;
+
+namespace BGB.Data.Entities.Wbpm
+{
+    [Table("ProjectWork", Schema = "wbpm")]
+    public class ProjectWork : BaseEntity
+    {
+        public ProjectWork()
+        {
+            ContractorCompanyPayments = new HashSet<ContractorCompanyPayment>();
+            ADPReceivePayments = new HashSet<ADPReceivePayment>();
+            BGBMiscellaneousFunds = new HashSet<BGBMiscellaneousFund>();
+        }
+
+
+        [Key]
+        public int ProjectWorkId { get; set; }
+
+        public int ADPProjectId { get; set; }
+        public int ConstructionCompanyId { get; set; }
+        public string ProjectWorkTitle { get; set; }
+        public decimal? EstimatedCost { get; set; }
+        public DateTime NOADate { get; set; }
+        public DateTime AgreementDate { get; set; }
+        public DateTime WorkOrderDate { get; set; }
+        public DateTime WorkOrderTime { get; set; }
+        public DateTime WorkStartDate { get; set; }
+        public DateTime WorkEndDate { get; set; }
+        public DateTime? OnFieldStartDate { get; set; }
+        public DateTime? OnFieldEndDate { get; set; }
+        public DateTime HandOverDate { get; set; }
+        public DateTime BankGuaranteeEndDate { get; set; }
+        public float ConstructionProgress { get; set; }
+        public string BankGuaranteeDocument { get; set; }
+        public string NOADocument { get; set; }
+        public string AgreementDocument { get; set; }
+        public string WorkOrderDocument { get; set; }
+        public string WorkStatus { get; set; }
+        public string Remarks { get; set; }
+
+        #region Navigation Properties
+        [ForeignKey(nameof(ADPProjectId))]
+        public virtual ADPProject ADPProject { get; set; }
+        [ForeignKey(nameof(ConstructionCompanyId))]
+        public virtual ConstructionCompany ConstructionCompany { get; set; }
+
+
+        public virtual ICollection<ContractorCompanyPayment> ContractorCompanyPayments { get; set; }
+        public virtual ICollection<ADPReceivePayment> ADPReceivePayments { get; set; }
+        public virtual ICollection<BGBMiscellaneousFund> BGBMiscellaneousFunds { get; set; }
+        #endregion
+    }
+}
