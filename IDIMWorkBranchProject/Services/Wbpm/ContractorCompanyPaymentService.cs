37ddc7dfebbde9560eb0220aa0e8fecd956191e9
khpolash <khpolashbd@gmail.com>
2025-01-13 12:15:25 +0600

    Add: Implement ContractorCompanyPaymentService
    - Created ContractorCompanyPaymentService to handle operations related to ContractorCompanyPayment.
    - Inherited from BaseService to leverage common database operations.
    - Utilized IDIMDBEntities context for database interactions.

---
+++ IDIMWorkBranchProject/Services/Wbpm/ContractorCompanyPaymentService.cs
@@ -0,0 +1,15 @@
+﻿using BGB.Data.Entities.Wbpm;
+using IDIMWorkBranchProject.Data.Database;
+using IDIMWorkBranchProject.Services.Base;
+
+namespace IDIMWorkBranchProject.Services.Wbpm
+{
+    public class ContractorCompanyPaymentService : BaseService<ContractorCompanyPayment>, IContractorCompanyPaymentService
+    {
+        public ContractorCompanyPaymentService(IDIMDBEntities context) : base(context)
+        {
+        }
+
+
+    }
+}
