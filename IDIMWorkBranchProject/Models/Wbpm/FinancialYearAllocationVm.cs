b9b01e6204fc01b5c13ab1eede7dc2eed23744bf
khpolash <khpolashbd@gmail.com>
2025-01-13 13:03:48 +0600

    Add: FinancialYearAllocationVm class for managing financial year allocations in ADP projects
    - Introduced `FinancialYearAllocationVm` class to handle the allocation of funds for specific fiscal years in ADP projects.
    - Contains fields for the project ID, project title, fiscal year, total allocation, and detailed allocations for ADP and RADP.
    - The class tracks the total budget allocation for a project and breaks it down into the ADP and RADP components.
    - Implements data validation via FluentValidation using `FinancialYearAllocationVmValidator`.
    - Provides a structured way to manage and validate the financial allocations and budgets for ADP projects across different fiscal years.

---
+++ IDIMWorkBranchProject/Models/Wbpm/FinancialYearAllocationVm.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using FluentValidation.Attributes;
+using IDIMWorkBranchProject.Models.Validation.Wbpm;
+
+namespace IDIMWorkBranchProject.Models.Wbpm
+{
+    [Validator(typeof(FinancialYearAllocationVmValidator))]
+    public class FinancialYearAllocationVm
+    {
+
+        public int FinancialYearAllocationId { get; set; }
+
+        [DisplayName("প্রকল্প আইডি")]
+        public int ADPProjectId { get; set; }
+
+        [DisplayName("প্রকল্পের শিরোনাম")]
+        public string ProjectTitle { get; set; }
+
+        [DisplayName("আর্থবছর")]
+        public int FiscalYearId { get; set; }
+
+        [DisplayName("আর্থবছর")]
+        public string FiscalYearDescription { get; set; }
+
+        [DisplayName("মোট বরাদ্দ")]
+        public float TotalAllocation { get; set; }
+
+        [DisplayName("এডিপি বরাদ্দ")]
+        public float ADPAllocation { get; set; }
+
+        [DisplayName("আরএডিপি বরাদ্দ")]
+        public float RADPAllocation { get; set; }
+
+    }
+}
