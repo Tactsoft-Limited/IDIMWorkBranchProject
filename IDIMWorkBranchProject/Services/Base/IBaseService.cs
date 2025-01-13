fadf9c90837ba0839c088b4cd21fd283c0383c2e
khpolash <khpolashbd@gmail.com>
2025-01-13 13:08:15 +0600

    Add: IBaseService<TEntity> interface for generic CRUD operations
    - Introduced the `IBaseService<TEntity>` interface to define contract for generic CRUD functionality.
    - Declared methods for creating (`CreateAsync`), reading (`GetAllAsync`, `GetByIdAsync`), updating (`UpdateAsync`), and deleting (`DeleteAsync`) entities.
    - Provides the foundation for creating service classes that manage entities in a consistent and reusable manner.
    - Promotes the use of asynchronous operations for handling database interactions efficiently.
    - Defines a common interface that can be implemented by service classes to provide CRUD operations across different entity types.
    - Improves maintainability and scalability by ensuring a consistent service layer across the application.

---
+++ IDIMWorkBranchProject/Services/Base/IBaseService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IDIMWorkBranchProject.Services.Base
+{
+    public interface IBaseService<TEntity> where TEntity : class
+    {
+        IQueryable<TEntity> GetQueryableEntities();
+        Task<TEntity> CreateAsync(TEntity entity);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(object id);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task<TEntity> DeleteAsync(object id);
+    }
+}
