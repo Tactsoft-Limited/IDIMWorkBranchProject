7c160f809e18aefdb9f963bc471e622714450bc8
khpolash <khpolashbd@gmail.com>
2025-01-13 12:33:17 +0600

    Add: Define IADPProjectService interface
    - Created IADPProjectService interface to extend IBaseService for ADPProject entity.
    - Added GetPagedAsync method for paginated retrieval of ADPProject data using ADPProjectSearchVm.
    - Ensured consistency with base service structure for ADPProject-specific operations.

---
+++ IDIMWorkBranchProject/Services/Wbpm/IADPProjectService.cs
@@ -0,0 +1,12 @@
+﻿using BGB.Data.Entities.Wbpm;
+using IDIMWorkBranchProject.Models.Wbpm;
+using IDIMWorkBranchProject.Services.Base;
+using System.Threading.Tasks;
+
+namespace IDIMWorkBranchProject.Services.Wbpm
+{
+    public interface IADPProjectService : IBaseService<ADPProject>
+    {
+        Task<object> GetPagedAsync(ADPProjectSearchVm model);
+    }
+}
