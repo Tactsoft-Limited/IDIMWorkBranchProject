bc05128ec73ea2ee0281b7e1c58c00ad0e7e7ba8
khpolash <khpolashbd@gmail.com>
2025-01-13 12:16:14 +0600

    Add: Implement GetAllByProjectId method in FinancialYearAllocationService
    - Added GetAllByProjectId method to retrieve all FinancialYearAllocations by a given project ID.
    - Utilized asynchronous database querying with Entity Framework's ToListAsync.
    - Integrated with existing FinancialYearAllocationService class for better project-specific data management.

---
+++ IDIMWorkBranchProject/Services/Wbpm/FinancialYearAllocationService.cs
@@ -0,0 +1,22 @@
+﻿using BGB.Data.Entities.Wbpm;
+using IDIMWorkBranchProject.Data.Database;
+using IDIMWorkBranchProject.Services.Base;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IDIMWorkBranchProject.Services.Wbpm
+{
+    public class FinancialYearAllocationService : BaseService<FinancialYearAllocation>, IFinancialYearAllocationService
+    {
+        public FinancialYearAllocationService(IDIMDBEntities context) : base(context)
+        {
+        }
+
+        public async Task<List<FinancialYearAllocation>> GetAllByProjectId(int id)
+        {
+            return await _context.FinancialYearAllocations.Where(x => x.ADPProjectId == id).ToListAsync();
+        }
+    }
+}
