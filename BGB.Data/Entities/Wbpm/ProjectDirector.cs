c8802eb81450066aa8c705aead10e49104cfa21a
khpolash <khpolashbd@gmail.com>
2025-01-13 12:45:01 +0600

    Add: Define ProjectDirector entity for ADPProject management
    - Created ProjectDirector class to manage project director information for each ADPProject.
    - Added fields for project director name, designation, joining/transfer dates, and associated document.
    - Established foreign key relationship with ADPProject entity.
    - Inherited from BaseEntity to include standard audit fields (created/updated timestamps and user information).
    - Introduced IsActive flag to track active status of project directors.

---
+++ BGB.Data/Entities/Wbpm/ProjectDirector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using BGB.Data.Entities.Base;
+
+namespace BGB.Data.Entities.Wbpm
+{
+    [Table("ProjectDirector", Schema = "wbpm")]
+    public class ProjectDirector : BaseEntity
+    {
+
+        [Key]
+        public int ProjectDirectorId { get; set; }
+        public int ADPProjectId { get; set; }
+        public string ProjectDirectorName { get; set; }
+        public string Designation { get; set; }
+        public DateTime? JoiningDate { get; set; }
+        public DateTime? TransferDate { get; set; }
+        public string PDDocument { get; set; }
+        public bool IsActive { get; set; }
+
+
+        #region  Navigation Property
+        [ForeignKey(nameof(ADPProjectId))]
+        public ADPProject ADPProjects { get; set; }
+        #endregion
+    }
+}
