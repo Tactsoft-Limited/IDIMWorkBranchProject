3d27b4a23a0f493d9e841d2aec73864accd8d5e9
khpolash <khpolashbd@gmail.com>
2025-01-13 12:36:25 +0600

    Add: Define IFinancialYearAllocationService interface
    - Created IFinancialYearAllocationService interface to extend IBaseService for FinancialYearAllocation entity.
    - Added GetAllByProjectId method for retrieving Financial

---
+++ IDIMWorkBranchProject/Services/Wbpm/IFinancialYearAllocationService.cs
@@ -0,0 +1,12 @@
+﻿using BGB.Data.Entities.Wbpm;
+using IDIMWorkBranchProject.Services.Base;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IDIMWorkBranchProject.Services.Wbpm
+{
+    public interface IFinancialYearAllocationService : IBaseService<FinancialYearAllocation>
+    {
+        Task<List<FinancialYearAllocation>> GetAllByProjectId(int id);
+    }
+}
