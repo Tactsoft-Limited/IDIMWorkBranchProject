3d8db0e95ce8bd2782f73bc35fe09081da00fb77
khpolash <khpolashbd@gmail.com>
2025-01-13 12:38:00 +0600

    Add: Define IProjectWorkService interface
    - Created IProjectWorkService interface to extend IBaseService for ProjectWork entity.
    - Added GetAllByProjectId method to retrieve ProjectWorks by project ID asynchronously.

---
+++ IDIMWorkBranchProject/Services/Wbpm/IProjectWorkService.cs
@@ -0,0 +1,13 @@
+﻿using BGB.Data.Entities.Wbpm;
+using IDIMWorkBranchProject.Models.Wbpm;
+using IDIMWorkBranchProject.Services.Base;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IDIMWorkBranchProject.Services.Wbpm
+{
+    public interface IProjectWorkService : IBaseService<ProjectWork>
+    {
+        Task<List<ProjectWork>> GetAllByProjectId(int id);
+    }
+}
