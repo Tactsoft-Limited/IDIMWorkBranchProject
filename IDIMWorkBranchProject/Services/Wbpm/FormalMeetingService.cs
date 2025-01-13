baf155845f5dae480059dbaa0c9313636379e2c0
khpolash <khpolashbd@gmail.com>
2025-01-13 12:32:37 +0600

    Add: Implement GetAllByProjectId method in FormalMeetingService
    - Added GetAllByProjectId method to retrieve all FormalMeetings by a given project ID.
    - Utilized asynchronous database querying with Entity Framework's ToListAsync.
    - Integrated with existing FormalMeetingService class to manage project-specific formal meetings data.

---
+++ IDIMWorkBranchProject/Services/Wbpm/FormalMeetingService.cs
@@ -0,0 +1,22 @@
+﻿using BGB.Data.Entities.Wbpm;
+using IDIMWorkBranchProject.Data.Database;
+using IDIMWorkBranchProject.Services.Base;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IDIMWorkBranchProject.Services.Wbpm
+{
+    public class FormalMeetingService : BaseService<FormalMeeting>, IFormalMeetingService
+    {
+        public FormalMeetingService(IDIMDBEntities context) : base(context)
+        {
+        }
+
+        public async Task<List<FormalMeeting>> GetAllByProjectId(int id)
+        {
+            return await _context.FormalMeetings.Where(x => x.ADPProjectId == id).ToListAsync();
+        }
+    }
+}
