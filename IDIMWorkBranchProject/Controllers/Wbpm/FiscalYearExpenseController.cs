c904737cd1a956cd3d801def7c32dec2b3833ed1
khpolash <khpolashbd@gmail.com>
2025-02-06 14:34:00 +0600

    Implement CRUD functionality in FiscalYearExpenseController
    - Added Create action methods for FiscalYearExpense with asynchronous data retrieval.
    - Integrated AutoMapper for model mapping from ViewModel to ADPProject entity.
    - Improved error handling and user feedback through TempData messages.
    - Redirected to ADPProject details upon successful creation.

---
+++ IDIMWorkBranchProject/Controllers/Wbpm/FiscalYearExpenseController.cs
@@ -0,0 +1,82 @@
+﻿using AutoMapper;
+using BGB.Data.Entities.Wbpm;
+
+
+using IDIMWorkBranchProject.Extentions;
+using IDIMWorkBranchProject.Models.Wbpm;
+using IDIMWorkBranchProject.Services;
+using IDIMWorkBranchProject.Services.Setup;
+using IDIMWorkBranchProject.Services.Wbpm;
+
+using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace IDIMWorkBranchProject.Controllers.Wbpm
+{
+	public class FiscalYearExpenseController : BaseController
+	{
+		protected readonly IFiscalYearExpenseService _fiscalYearExpenseService;
+		protected readonly IMapper _mapper;
+		protected readonly IADPProjectService _aDPProjectService;
+		protected readonly IFiscalYearService _fiscalYearService;
+
+		public FiscalYearExpenseController(IActivityLogService activityLogService, IFiscalYearExpenseService fiscalYearExpenseService, IMapper mapper) : base(activityLogService)
+		{
+			_fiscalYearExpenseService = fiscalYearExpenseService;
+			_mapper = mapper;
+		}
+
+
+		// GET: FiscalYearExpense
+		public ActionResult Index()
+		{
+			return RedirectToAction("List");
+		}
+
+		public ActionResult List()
+		{
+			var model = new FiscalYearExpenseSearchVm();
+			return View(model);
+		}
+
+		public async Task<ActionResult> Create(int id)
+		{
+			var data = await _aDPProjectService.GetByIdAsync(id);
+
+			var model = new FiscalYearExpenseVm
+			{
+				ADPProjectId = data.ADPProjectId,
+				ProjectTitle = data.ProjectTitle,
+				FiscalYearDropdown = await _fiscalYearService.GetDropdownAsync()
+			};
+			
+			return View(model);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult> Create(FiscalYearExpenseVm model)
+		{
+			try
+			{
+				if (!ModelState.IsValid)
+				{
+					TempData["Message"] = Messages.InvalidInput(MessageType.Create.ToString());
+					return View(model);
+				}
+
+				var entity = _mapper.Map<ADPProject>(model);
+				await _aDPProjectService.CreateAsync(entity);
+				TempData["Message"] = Messages.Success(MessageType.Create.ToString());
+				return RedirectToAction("Details/"+model.ADPProjectId, "ADPProject");
+			}
+			catch (Exception exception)
+			{
+				TempData["Message"] = Messages.Failed(MessageType.Create.ToString(), exception.Message);
+				return View(model);
+			}
+		}
+
+	}
+}
