ada22334b6e11a82e1fb19dade8e4466a8cb960a
khpolash <khpolashbd@gmail.com>
2025-02-06 15:43:40 +0600

    Refactor BaseEntity Class: Change CreatedUser and CreatedDateTime to non-nullable types
    - Updated CreatedUser and CreatedDateTime properties to non-nullable types for consistency.
    - This change ensures that these fields will always have a value, preventing potential null reference issues.
    - Adjusted related code to accommodate the new non-nullable types.

--- BGB.Data/Entities/Base/BaseEntity.cs
+++ BGB.Data/Entities/Base/BaseEntity.cs
@@ -4,11 +4,11 @@ namespace BGB.Data.Entities.Base
 {
     public class BaseEntity
     {
-        public int? CreatedUser { get; set; }
-        public DateTime? CreatedDateTime { get; set; }
+        public int CreatedUser { get; set; }
+        public DateTime CreatedDateTime { get; set; }
         public int? UpdatedUser { get; set; }
         public DateTime? UpdatedDateTime { get; set; }
-        public int? UpdateNo { get; set; }
-    }
+        public int UpdateNo { get; set; }
+	}
 
 }
