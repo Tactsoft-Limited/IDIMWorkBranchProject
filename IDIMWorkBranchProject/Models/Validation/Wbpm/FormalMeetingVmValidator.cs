0a4ff146846e58ced13083867f874920cd44aadd
khpolash <khpolashbd@gmail.com>
2025-02-08 14:04:53 +0600

    Refactor FormalMeetingVmValidator
    - Removed future date validation for MeetingDate.
    - Cleaned up comments related to MeetingDocument and MeetingDocumentFile validation.
    - Ensured essential validation rules remain intact.

--- IDIMWorkBranchProject/Models/Validation/Wbpm/FormalMeetingVmValidator.cs
+++ IDIMWorkBranchProject/Models/Validation/Wbpm/FormalMeetingVmValidator.cs
@@ -21,8 +21,7 @@ namespace IDIMWorkBranchProject.Models.Validation.Wbpm
                 .GreaterThan(0).WithMessage("মিটিং সংখ্যা অবশ্যই ১ বা তার বেশি হতে হবে।");
 
             RuleFor(x => x.MeetingDate)
-                .NotEmpty().WithMessage("মিটিং তারিখ অবশ্যই পূর্ণ করতে হবে।")
-                .LessThanOrEqualTo(DateTime.Now).WithMessage("মিটিং তারিখ ভবিষ্যতের হতে পারে না।");
+                .NotEmpty().WithMessage("মিটিং তারিখ অবশ্যই পূর্ণ করতে হবে।");
 
             //RuleFor(x => x.MeetingDocument)
             //    .Length(1, 150).WithMessage("মিটিং ডকুমেন্ট নাম ১ থেকে ১৫০ অক্ষরের মধ্যে হতে হবে।");
