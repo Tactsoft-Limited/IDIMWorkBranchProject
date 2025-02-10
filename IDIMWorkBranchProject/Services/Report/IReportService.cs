9b240af8f924b5e0ce237651d13d6202a562efe1
khpolash <khpolashbd@gmail.com>
2025-02-10 17:57:05 +0600

    add report service with interface

---
+++ IDIMWorkBranchProject/Services/Report/IReportService.cs
@@ -0,0 +1,15 @@
+﻿using BGB.Data.SqlViews.Wbpm;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDIMWorkBranchProject.Services.Report{
+	public interface IReportService
+	{
+		Task<ViewADPReceivePayment> GetADPReceivePaymenAsync(int id);
+	}
+}
+
