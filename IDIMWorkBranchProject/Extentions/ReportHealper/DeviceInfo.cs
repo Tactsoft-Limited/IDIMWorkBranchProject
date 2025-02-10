afba0747295be0d4621f128a0ed6e2e05a874873
khpolash <khpolashbd@gmail.com>
2025-02-10 17:56:39 +0600

    add report

---
+++ IDIMWorkBranchProject/Extentions/ReportHealper/DeviceInfo.cs
@@ -0,0 +1,77 @@
+﻿namespace IDIMWorkBranchProject.Extentions.ReportHelper
+{
+	public class DeviceInfo : IDeviceInfo
+	{
+		public string OutputFormat { get; set; }
+
+		public DeviceInfo(string outputFormat = "PDF")
+		{
+			OutputFormat = outputFormat;
+		}
+
+		public string Landscape()
+		{
+			DeviceInfoModel infoModel = new DeviceInfoModel
+			{
+				OutputFormat = OutputFormat,
+				PageWidth = "11.69in",
+				PageHeight = "8.27in",
+				MarginTop = "0.5in",
+				MarginLeft = "0.5in",
+				MarginRight = "0.5in",
+				MarginBottom = "0.5in"
+			};
+			return Custom(infoModel);
+		}
+
+		public string Portrait()
+		{
+			DeviceInfoModel infoModel = new DeviceInfoModel
+			{
+				OutputFormat = OutputFormat,
+				PageWidth = "8.27in",
+				PageHeight = "11.69in",
+				MarginTop = ".25in",
+				MarginLeft = ".5in",
+				MarginRight = ".5in",
+				MarginBottom = ".25in"
+			};
+			return Custom(infoModel);
+		}
+
+		public string LegalPortrait()
+		{
+			DeviceInfoModel infoModel = new DeviceInfoModel
+			{
+				OutputFormat = OutputFormat,
+				PageWidth = "8.5in",
+				PageHeight = "14in",
+				MarginTop = "0.5in",
+				MarginLeft = ".5in",
+				MarginRight = ".5in",
+				MarginBottom = "0.5in"
+			};
+			return Custom(infoModel);
+		}
+
+		public string LegalLandscape()
+		{
+			DeviceInfoModel infoModel = new DeviceInfoModel
+			{
+				OutputFormat = OutputFormat,
+				PageWidth = "14in",
+				PageHeight = "8.5in",
+				MarginTop = "0.5in",
+				MarginLeft = ".5in",
+				MarginRight = ".5in",
+				MarginBottom = "0.5in"
+			};
+			return Custom(infoModel);
+		}
+
+		public string Custom(DeviceInfoModel infoModel)
+		{
+			return "<DeviceInfo>  <OutputFormat>" + OutputFormat + "</OutputFormat>  <PageWidth>" + infoModel.PageWidth + "</PageWidth>  <PageHeight>" + infoModel.PageHeight + "</PageHeight>  <MarginTop>" + infoModel.MarginTop + "</MarginTop>  <MarginLeft>" + infoModel.MarginLeft + "</MarginLeft>  <MarginRight>" + infoModel.MarginRight + "</MarginRight>  <MarginBottom>" + infoModel.MarginBottom + "</MarginBottom></DeviceInfo>";
+		}
+	}
+}
---
+++ IDIMWorkBranchProject/Extentions/ReportHealper/ReportResult.cs
@@ -0,0 +1,59 @@
+﻿using IDIMWorkBranchProject.Extentions.ReportHelper;
+
+using Microsoft.Reporting.WebForms;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IDIMWorkBranchProject.Extentions.ReportHealper
+{
+	public class ReportResult : ActionResult, IReportResult
+	{
+		private byte[] ByteArray { get; set; }
+
+		private ReportConfig Config { get; set; }
+
+		private ReportDataSource ReportDataSource { get; set; }
+
+		public string MimeType { get; set; }
+
+		public string FileExtention { get; set; }
+
+		public ReportResult(ReportConfig config, List<ReportDataSource> reportDataSource)
+		{
+			Config = config;
+			if (!File.Exists(config.ReportFilePath))
+			{
+				throw new FileNotFoundException("report path not found.");
+			}
+
+			LocalReport localReport = new LocalReport
+			{
+				ReportPath = config.ReportFilePath
+			};
+			foreach (ReportDataSource item in reportDataSource)
+			{
+				localReport.DataSources.Add(item);
+			}
+
+			ByteArray = localReport.Render(Config.ReportType, Config.DeviceInfo, out var mimeType, out var _, out var fileNameExtension, out var _, out var _);
+			MimeType = mimeType;
+			FileExtention = fileNameExtension;
+		}
+
+		public override void ExecuteResult(ControllerContext context)
+		{
+			HttpResponseBase response = context.HttpContext.Response;
+			response.Clear();
+			if (Config.ReportType.ToLower() != "pdf" || Config.IsDownloadable)
+			{
+				response.AddHeader("content-disposition", "attachment; filename=" + Config.FileName + "." + FileExtention);
+			}
+
+			response.ContentType = MimeType;
+			response.BinaryWrite(ByteArray);
+		}
+	}
+}
