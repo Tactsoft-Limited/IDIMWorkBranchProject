1ab3b5c9303c9290dd57d501cdf4397839bb5a22
khpolash <khpolashbd@gmail.com>
2025-02-12 16:44:02 +0600

    Add ReportConfig class for report generation configuration
    - Created `ReportConfig` class to encapsulate configuration details for report generation, such as file name, data source, report type, and device information.
    - Automatically sets default values for report type, download options, and file name based on current request parameters.
    - Integrates device info configuration (such as portrait layout) based on the specified report type.
    - Supports configurable report generation options for both PDF and Excel formats.

---
+++ IDIMWorkBranchProject/Extentions/ReportHelper/ReportConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Data;
+using System.Web;
+using System;
+
+namespace IDIMWorkBranchProject.Extentions.ReportHelper
+{
+	public class ReportConfig
+	{
+		public string FileName { get; set; }
+
+		public DataTable DataTable { get; set; }
+
+		public string ReportFilePath { get; set; }
+
+		public string ReportSourceName { get; set; }
+
+		public string ReportType { get; set; }
+
+		public string DeviceInfo { get; set; }
+
+		public string PageType { get; set; }
+
+		public bool IsDownloadable { get; set; }
+
+		public HttpRequestBase Request { get; set; } = new HttpRequestWrapper(HttpContext.Current.Request);
+
+
+		public ReportConfig()
+		{
+			DeviceInfo = new DeviceInfo(ReportType).Portrait();
+			FileName = $"{DateTime.UtcNow.AddHours(6.0):ddMMyyyyHHmmss}";
+			ReportType = ReportType ?? Request.Params["type"] ?? "pdf";
+			ReportType = ((ReportType != "excel") ? "pdf" : ReportType);
+			IsDownloadable = Request.Params["download"] == "true";
+		}
+	}
+}
