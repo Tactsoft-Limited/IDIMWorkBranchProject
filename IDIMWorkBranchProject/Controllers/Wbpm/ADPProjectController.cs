3fd50deda054a719ca86fa828aa0b0c1d2aa66da
khpolash <khpolashbd@gmail.com>
2025-02-12 16:40:51 +0600

    Refactor ADPProjectController: - Consolidated repeated code by ensuring consistency in action methods. - Enhanced error handling and message display on create and edit actions. - Updated logic to maintain consistency in model mapping and response handling. - Added redirect on successful update in the Edit action to improve user flow.

--- IDIMWorkBranchProject/Controllers/Wbpm/ADPProjectController.cs
+++ IDIMWorkBranchProject/Controllers/Wbpm/ADPProjectController.cs
@@ -1,12 +1,9 @@
 ﻿using AutoMapper;
-
 using BGB.Data.Entities.Wbpm;
-
 using IDIMWorkBranchProject.Extentions;
 using IDIMWorkBranchProject.Models.Wbpm;
 using IDIMWorkBranchProject.Services;
 using IDIMWorkBranchProject.Services.Wbpm;
-
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -14,7 +11,7 @@ using System.Web.Mvc;
 
 namespace IDIMWorkBranchProject.Controllers.Wbpm
 {
-	public class ADPProjectController : BaseController
+    public class ADPProjectController : BaseController
     {
         private readonly IADPProjectService _aDPProjectService;
         private readonly IProjectDirectorService _projectDirectorService;
