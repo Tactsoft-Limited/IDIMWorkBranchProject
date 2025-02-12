cdbfc38ef7cbd58d70a289965a5e0ecd14842554
khpolash <khpolashbd@gmail.com>
2025-02-12 16:49:23 +0600

    Implement ReportService with ADPReceivePayment retrieval
    - Created `ReportService` class to handle report-related logic.
    - Added `GetADPReceivePaymenAsync` method to fetch `ViewADPReceivePayment` data by ID.
    - Integrated AutoMapper for potential mapping needs in future enhancements.
    - Used asynchronous database calls with `ToListAsync` for efficient data retrieval.

--- IDIMWorkBranchProject/Services/Report/IReportService.cs
+++ IDIMWorkBranchProject/Services/Report/IReportService.cs
@@ -6,10 +6,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IDIMWorkBranchProject.Services.Report{
-	public interface IReportService
-	{
-		Task<ViewADPReceivePayment> GetADPReceivePaymenAsync(int id);
-	}
+namespace IDIMWorkBranchProject.Services.Report
+{
+    public interface IReportService
+    {
+        Task<List<ViewADPReceivePayment>> GetADPReceivePaymenAsync(int id);
+    }
 }
 
--- IDIMWorkBranchProject/Services/Report/ReportService.cs
+++ IDIMWorkBranchProject/Services/Report/ReportService.cs
@@ -3,27 +3,27 @@
 using BGB.Data.SqlViews.Wbpm;
 
 using IDIMWorkBranchProject.Data.Database;
-
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace IDIMWorkBranchProject.Services.Report
 {
-	public class ReportService : IReportService
-	{
-		protected readonly IDIMDBEntities _context;
-		protected readonly IMapper _mapper;
+    public class ReportService : IReportService
+    {
+        protected readonly IDIMDBEntities _context;
+        protected readonly IMapper _mapper;
 
-		public ReportService(IDIMDBEntities context, IMapper mapper)
-		{
-			_context = context;
-			_mapper = mapper;
-		}
+        public ReportService(IDIMDBEntities context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
 
-		public async Task<ViewADPReceivePayment> GetADPReceivePaymenAsync(int id)
-		{
-			return await _context.ViewADPReceivePayments.Where(x=>x.ADPReceivePaymentId == id).FirstOrDefaultAsync();
-		}
-	}
+        public async Task<List<ViewADPReceivePayment>> GetADPReceivePaymenAsync(int id)
+        {
+            return await _context.ViewADPReceivePayments.Where(x => x.ADPReceivePaymentId == id).ToListAsync();
+        }
+    }
 }
