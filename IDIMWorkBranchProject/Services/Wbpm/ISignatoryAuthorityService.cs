4021c425c898300925c79b20ab02b8a08ec9d51c
khpolash <khpolashbd@gmail.com>
2025-02-18 15:04:44 +0600

    fixed namespace issue

--- IDIMWorkBranchProject/Services/Wbpm/ISignatoryAuthorityService.cs
+++ IDIMWorkBranchProject/Services/Wbpm/ISignatoryAuthorityService.cs
@@ -1,4 +1,5 @@
 ﻿using BGB.Data.Entities.Wbpm;
+using IDIMWorkBranchProject.Models.Wbpm;
 using IDIMWorkBranchProject.Services.Base;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace IDIMWorkBranchProject.Services.Wbpm
 {
     public interface ISignatoryAuthorityService : IBaseService<SignatoryAuthority>
     {
-       // Task<object> GetByAsync(SignatoryAuthoritySearchVm filter = null);
+        // Task<object> GetByAsync(SignatoryAuthoritySearchVm filter = null);
         Task<IEnumerable<SelectListItem>> GetDropdownAsync(int? selected = 0);
 
         Task<object> GetPagedAsync(SignatoryAuthoritySearchVm model);
