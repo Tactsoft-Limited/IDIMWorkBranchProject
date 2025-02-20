74e588341fa5042b21d9d7696229a3d669e534ba
khpolash <khpolashbd@gmail.com>
2025-02-20 13:44:19 +0600

    Refactor ReportConfig constructor to handle multiple report types - Modified ReportType assignment logic to handle "excel" and "word" types in addition to "pdf". - Moved DeviceInfo initialization to the end of the constructor for better clarity and consistency.

--- IDIMWorkBranchProject/Extentions/ReportHealper/ReportConfig.cs
+++ IDIMWorkBranchProject/Extentions/ReportHealper/ReportConfig.cs
@@ -4,34 +4,25 @@ using System;
 
 namespace IDIMWorkBranchProject.Extentions.ReportHelper
 {
-	public class ReportConfig
-	{
-		public string FileName { get; set; }
+    public class ReportConfig
+    {
+        public string FileName { get; set; }
+        public DataTable DataTable { get; set; }
+        public string ReportFilePath { get; set; }
+        public string ReportSourceName { get; set; }
+        public string ReportType { get; set; }
+        public string DeviceInfo { get; set; }
+        public string PageType { get; set; }
+        public bool IsDownloadable { get; set; }
+        public HttpRequestBase Request { get; set; } = new HttpRequestWrapper(HttpContext.Current.Request);
 
-		public DataTable DataTable { get; set; }
-
-		public string ReportFilePath { get; set; }
-
-		public string ReportSourceName { get; set; }
-
-		public string ReportType { get; set; }
-
-		public string DeviceInfo { get; set; }
-
-		public string PageType { get; set; }
-
-		public bool IsDownloadable { get; set; }
-
-		public HttpRequestBase Request { get; set; } = new HttpRequestWrapper(HttpContext.Current.Request);
-
-
-		public ReportConfig()
-		{
-			DeviceInfo = new DeviceInfo(ReportType).Portrait();
-			FileName = $"{DateTime.UtcNow.AddHours(6.0):ddMMyyyyHHmmss}";
-			ReportType = ReportType ?? Request.Params["type"] ?? "pdf";
-			ReportType = ((ReportType != "excel") ? "pdf" : ReportType);
-			IsDownloadable = Request.Params["download"] == "true";
-		}
-	}
+        public ReportConfig()
+        {
+            ReportType = ReportType ?? Request.Params["type"] ?? "pdf";
+            ReportType = (ReportType != "excel" && ReportType != "word") ? "pdf" : ReportType;
+            IsDownloadable = Request.Params["download"] == "true";
+            DeviceInfo = new DeviceInfo(ReportType).Portrait();
+            FileName = $"{DateTime.UtcNow.AddHours(6.0):ddMMyyyyHHmmss}";
+        }
+    }
 }
--- IDIMWorkBranchProject/Extentions/ReportHealper/ReportResult.cs
+++ IDIMWorkBranchProject/Extentions/ReportHealper/ReportResult.cs
@@ -1,7 +1,6 @@
 ﻿using IDIMWorkBranchProject.Extentions.ReportHelper;
-
 using Microsoft.Reporting.WebForms;
-
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Web;
@@ -9,51 +8,48 @@ using System.Web.Mvc;
 
 namespace IDIMWorkBranchProject.Extentions.ReportHealper
 {
-	public class ReportResult : ActionResult, IReportResult
-	{
-		private byte[] ByteArray { get; set; }
+    public class ReportResult : ActionResult, IReportResult
+    {
+        private byte[] ByteArray { get; set; }
+        private ReportConfig Config { get; set; }
+        public string MimeType { get; set; }
+        public string FileExtention { get; set; }
 
-		private ReportConfig Config { get; set; }
+        public ReportResult(ReportConfig config, List<ReportDataSource> reportDataSource)
+        {
+            Config = config ?? throw new ArgumentNullException(nameof(config));
 
-		private ReportDataSource ReportDataSource { get; set; }
+            if (!File.Exists(config.ReportFilePath))
+            {
+                throw new FileNotFoundException("report path not found.");
+            }
 
-		public string MimeType { get; set; }
+            var localReport = new LocalReport
+            {
+                ReportPath = config.ReportFilePath
+            };
 
-		public string FileExtention { get; set; }
+            foreach (ReportDataSource item in reportDataSource)
+            {
+                localReport.DataSources.Add(item);
+            }
 
-		public ReportResult(ReportConfig config, List<ReportDataSource> reportDataSource)
-		{
-			Config = config;
-			if (!File.Exists(config.ReportFilePath))
-			{
-				throw new FileNotFoundException("report path not found.");
-			}
+            ByteArray = localReport.Render(Config.ReportType, Config.DeviceInfo, out var mimeType, out var _, out var fileNameExtension, out var _, out var _);
+            MimeType = mimeType;
+            FileExtention = fileNameExtension;
+        }
 
-			LocalReport localReport = new LocalReport
-			{
-				ReportPath = config.ReportFilePath
-			};
-			foreach (ReportDataSource item in reportDataSource)
-			{
-				localReport.DataSources.Add(item);
-			}
+        public override void ExecuteResult(ControllerContext context)
+        {
+            HttpResponseBase response = context.HttpContext.Response;
+            response.Clear();
+            if (Config.ReportType.ToLower() != "pdf" || Config.IsDownloadable)
+            {
+                response.AddHeader("content-disposition", "attachment; filename=" + Config.FileName + "." + FileExtention);
+            }
 
-			ByteArray = localReport.Render(Config.ReportType, Config.DeviceInfo, out var mimeType, out var _, out var fileNameExtension, out var _, out var _);
-			MimeType = mimeType;
-			FileExtention = fileNameExtension;
-		}
-
-		public override void ExecuteResult(ControllerContext context)
-		{
-			HttpResponseBase response = context.HttpContext.Response;
-			response.Clear();
-			if (Config.ReportType.ToLower() != "pdf" || Config.IsDownloadable)
-			{
-				response.AddHeader("content-disposition", "attachment; filename=" + Config.FileName + "." + FileExtention);
-			}
-
-			response.ContentType = MimeType;
-			response.BinaryWrite(ByteArray);
-		}
-	}
+            response.ContentType = MimeType;
+            response.BinaryWrite(ByteArray);
+        }
+    }
 }
