82544ef7e81288a5080039d3a4d1259535a25b32
khpolash <khpolashbd@gmail.com>
2025-02-20 13:40:45 +0600

    Refactor DeviceInfo class to simplify page layout methods
    - Replaced individual methods for each page layout (Landscape, Portrait, etc.) with a single helper method (GetDeviceInfo) to reduce code duplication.
    - Updated the DeviceInfo class to use string interpolation for building XML output in the Custom method.

--- IDIMWorkBranchProject/Extentions/ReportHealper/DeviceInfo.cs
+++ IDIMWorkBranchProject/Extentions/ReportHealper/DeviceInfo.cs
@@ -1,77 +1,50 @@
 ﻿namespace IDIMWorkBranchProject.Extentions.ReportHelper
 {
-	public class DeviceInfo : IDeviceInfo
-	{
-		public string OutputFormat { get; set; }
+    public class DeviceInfo : IDeviceInfo
+    {
+        public string OutputFormat { get; set; }
 
-		public DeviceInfo(string outputFormat = "PDF")
-		{
-			OutputFormat = outputFormat;
-		}
+        public DeviceInfo(string outputFormat = "PDF")
+        {
+            OutputFormat = outputFormat;
+        }
 
-		public string Landscape()
-		{
-			DeviceInfoModel infoModel = new DeviceInfoModel
-			{
-				OutputFormat = OutputFormat,
-				PageWidth = "11.69in",
-				PageHeight = "8.27in",
-				MarginTop = "0.5in",
-				MarginLeft = "0.5in",
-				MarginRight = "0.5in",
-				MarginBottom = "0.5in"
-			};
-			return Custom(infoModel);
-		}
+        public string Landscape() => GetDeviceInfo("11.69in", "8.27in", "0.5in", "0.5in", "0.5in", "0.5in");
 
-		public string Portrait()
-		{
-			DeviceInfoModel infoModel = new DeviceInfoModel
-			{
-				OutputFormat = OutputFormat,
-				PageWidth = "8.27in",
-				PageHeight = "11.69in",
-				MarginTop = ".25in",
-				MarginLeft = ".5in",
-				MarginRight = ".5in",
-				MarginBottom = ".25in"
-			};
-			return Custom(infoModel);
-		}
+        public string Portrait() => GetDeviceInfo("8.27in", "11.69in", ".25in", ".5in", ".5in", ".25in");
 
-		public string LegalPortrait()
-		{
-			DeviceInfoModel infoModel = new DeviceInfoModel
-			{
-				OutputFormat = OutputFormat,
-				PageWidth = "8.5in",
-				PageHeight = "14in",
-				MarginTop = "0.5in",
-				MarginLeft = ".5in",
-				MarginRight = ".5in",
-				MarginBottom = "0.5in"
-			};
-			return Custom(infoModel);
-		}
+        public string LegalPortrait() => GetDeviceInfo("8.5in", "14in", "0.5in", ".5in", ".5in", "0.5in");
 
-		public string LegalLandscape()
-		{
-			DeviceInfoModel infoModel = new DeviceInfoModel
-			{
-				OutputFormat = OutputFormat,
-				PageWidth = "14in",
-				PageHeight = "8.5in",
-				MarginTop = "0.5in",
-				MarginLeft = ".5in",
-				MarginRight = ".5in",
-				MarginBottom = "0.5in"
-			};
-			return Custom(infoModel);
-		}
+        public string LegalLandscape() => GetDeviceInfo("14in", "8.5in", "0.5in", ".5in", ".5in", "0.5in");
 
-		public string Custom(DeviceInfoModel infoModel)
-		{
-			return "<DeviceInfo>  <OutputFormat>" + OutputFormat + "</OutputFormat>  <PageWidth>" + infoModel.PageWidth + "</PageWidth>  <PageHeight>" + infoModel.PageHeight + "</PageHeight>  <MarginTop>" + infoModel.MarginTop + "</MarginTop>  <MarginLeft>" + infoModel.MarginLeft + "</MarginLeft>  <MarginRight>" + infoModel.MarginRight + "</MarginRight>  <MarginBottom>" + infoModel.MarginBottom + "</MarginBottom></DeviceInfo>";
-		}
-	}
+        private string GetDeviceInfo(string pageWidth, string pageHeight, string marginTop, string marginLeft, string marginRight, string marginBottom)
+        {
+            var infoModel = new DeviceInfoModel
+            {
+                OutputFormat = OutputFormat,
+                PageWidth = pageWidth,
+                PageHeight = pageHeight,
+                MarginTop = marginTop,
+                MarginLeft = marginLeft,
+                MarginRight = marginRight,
+                MarginBottom = marginBottom
+            };
+            return Custom(infoModel);
+        }
+
+        public string Custom(DeviceInfoModel infoModel)
+        {
+            return
+                $"<DeviceInfo>" +
+                $"<OutputFormat>{OutputFormat}</OutputFormat>" +
+                $"<PageWidth>{infoModel.PageWidth}</PageWidth>" +
+                $"<PageHeight>{infoModel.PageHeight}</PageHeight>" +
+                $"<MarginTop>{infoModel.MarginTop}</MarginTop>" +
+                $"<MarginLeft>{infoModel.MarginLeft}</MarginLeft>" +
+                $"<MarginRight>{infoModel.MarginRight}</MarginRight>" +
+                $"<MarginBottom>{infoModel.MarginBottom}</MarginBottom>" +
+                $"</DeviceInfo>";
+        }
+    }
+
 }
