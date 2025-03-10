5ce3bf79739792b85eb554d27c931ef8757e0ab9
khpolash <khpolashbd@gmail.com>
2025-03-10 11:26:08 +0600

    Added IWorkOrderService interface and WorkOrderService class with GetByProjectWorkIdAsync method to fetch WorkOrder by ProjectWorkId

---
+++ IDIMWorkBranchProject/Services/Wbpm/IWorkOrderService.cs
@@ -0,0 +1,11 @@
+﻿using BGB.Data.Entities.Wbpm;
+using IDIMWorkBranchProject.Services.Base;
+using System.Threading.Tasks;
+
+namespace IDIMWorkBranchProject.Services.Wbpm
+{
+    public interface IWorkOrderService : IBaseService<WorkOrder>
+    {
+        Task<WorkOrder> GetByProjectWorkIdAsync(int id);
+    }
+}
---
+++ IDIMWorkBranchProject/Services/Wbpm/WorkOrderService.cs
@@ -0,0 +1,21 @@
+﻿using BGB.Data.Entities.Wbpm;
+using IDIMWorkBranchProject.Data.Database;
+using IDIMWorkBranchProject.Services.Base;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IDIMWorkBranchProject.Services.Wbpm
+{
+    public class WorkOrderService : BaseService<WorkOrder>, IWorkOrderService
+    {
+        public WorkOrderService(IDIMDBEntities context) : base(context)
+        {
+        }
+
+        public async Task<WorkOrder> GetByProjectWorkIdAsync(int id)
+        {
+            return await _context.WorkOrders.Where(x => x.ProjectWorkId == id).FirstOrDefaultAsync();
+        }
+    }
+}
