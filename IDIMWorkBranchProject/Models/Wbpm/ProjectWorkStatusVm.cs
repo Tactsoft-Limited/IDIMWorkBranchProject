19f5e369cbf0c3b99d97a4446523e0adfc383b16
khpolash <khpolashbd@gmail.com>
2025-03-10 11:22:31 +0600

    Added ProjectWorkStatusVm view model with status tracking and corresponding enum for project work statuses

---
+++ IDIMWorkBranchProject/Models/Wbpm/ProjectWorkStatusVm.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace IDIMWorkBranchProject.Models.Wbpm
+{
+    public class ProjectWorkStatusVm
+    {
+        public int ProjectWorkStatusId { get; set; }
+
+        [DisplayName("নির্মাণ কাজ")]
+        public int ProjectWorkId { get; set; }
+
+        [DisplayName("নির্মাণ কাজের নাম (বাংলা)")]
+        public string ProjectWorkTitleB { get; set; }
+
+        [DisplayName("প্রকল্পের অবস্থা")]
+        public StatusType? StatusTypeId { get; set; }
+
+    }
+
+    public enum StatusType
+    {
+        Ongoing = 1,
+        Completed = 2,
+        Suspended = 3
+    }
+}
+
