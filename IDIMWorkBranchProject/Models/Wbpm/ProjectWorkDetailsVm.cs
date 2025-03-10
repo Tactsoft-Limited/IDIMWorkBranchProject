44b42470e60bc4e36fa77b8fc6da95a3c93d5939
khpolash <khpolashbd@gmail.com>
2025-03-10 14:10:09 +0600

    Update ProjectWorkDetailsVm structure to include additional project details
    - Expanded ProjectWorkDetailsVm to include specific fields like ProjectTitle, EstimatedCost, and status-related properties
    - Replaced the previous structure by incorporating more detailed project-related fields such as StartDate, EndDate, and NohaDate
    - Enhanced the model to better capture project work status and payment-related information (e.g., ADPReceivePayments, ContractorCompanyPayments)

--- IDIMWorkBranchProject/Models/Wbpm/ProjectWorkDetailsVm.cs
+++ IDIMWorkBranchProject/Models/Wbpm/ProjectWorkDetailsVm.cs
@@ -1,15 +1,33 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace IDIMWorkBranchProject.Models.Wbpm
 {
     public class ProjectWorkDetailsVm
     {
-        public ProjectWorkVm ProjectWorks { get; set; }
-        public NohaVm Noha { get; set; }
-        public PerformanceSecurityVm PerformanceSecurity { get; set; }
-        public ContractAgreementVm ContractAgreement { get; set; }
-        public WorkOrderVm WorkOrder { get; set; }
-        public ProjectWorkStatusVm ProjectWorkStatus { get; set; }
+        public int ADPProjectId { get; set; }
+        public string ProjectTitle { get; set; }
+        public int ProjectWorkId { get; set; }
+        public string ProjectWorkTitleB { get; set; }
+        public string ProjectWorkTitle { get; set; }
+        public decimal? EstimatedCost { get; set; }
+        public string EstimatedCostInWord { get; set; }
+        public bool IsNoahCompleted { get; set; }
+        public bool IsPerformanceSecuritySubmited { get; set; }
+        public bool IsAgreementCompleted { get; set; }
+        public bool IsWorkOrderCompleted { get; set; }
+        public string FirmNameB { get; set; }
+        public DateTime? AgreementDate { get; set; }
+        public DateTime? WorkOrderDate { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int? StatusTypeId { get; set; }
+        public DateTime? NohaDate { get; set; }
+        public DateTime? ExpiryDate { get; set; }
+        public int? ProjectWorkStatus { get; set; }
+        public string Remarks { get; set; }
+
+
         public List<ADPReceivePaymentVm> ADPReceivePayments { get; set; }
         public List<ContractorCompanyPaymentVm> ContractorCompanyPayments { get; set; }
 
