70c638ccc25201d565cb57cbf0be8e4003dc027b
khpolash <khpolashbd@gmail.com>
2025-03-10 11:23:48 +0600

    Implemented INohaService interface and NohaService class with GetByProjectWorkIdAsync method for fetching Noha by ProjectWorkId

---
+++ IDIMWorkBranchProject/Services/Wbpm/NohaService.cs
@@ -0,0 +1,21 @@
+﻿using BGB.Data.Entities.Wbpm;
+using IDIMWorkBranchProject.Data.Database;
+using IDIMWorkBranchProject.Services.Base;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IDIMWorkBranchProject.Services.Wbpm
+{
+    public class NohaService : BaseService<Noha>, INohaService
+    {
+        public NohaService(IDIMDBEntities context) : base(context)
+        {
+        }
+
+        public async Task<Noha> GetByProjectWorkIdAsync(int id)
+        {
+            return await _context.Nohas.Where(x => x.ProjectWorkId == id).FirstOrDefaultAsync();
+        }
+    }
+}
