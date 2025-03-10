f49a96fe4c065006b7818d11774c31133e86c309
khpolash <khpolashbd@gmail.com>
2025-03-10 14:09:39 +0600

    Update ADPProjectDetailVm to use ProjectWorkDetailsVm instead of ProjectWorkVm
    - Replaced ProjectWorkVm with ProjectWorkDetailsVm in the ProjectWorks property
    - Updated ADPProjectDetailVm model for better alignment with detailed project work data

--- IDIMWorkBranchProject/Models/Wbpm/ADPProjectDetailVm.cs
+++ IDIMWorkBranchProject/Models/Wbpm/ADPProjectDetailVm.cs
@@ -2,14 +2,14 @@
 
 namespace IDIMWorkBranchProject.Models.Wbpm
 {
-	public class ADPProjectDetailVm
+    public class ADPProjectDetailVm
     {
         public ADPProjectVm ADPProjectDetail { get; set; }
         public List<ProjectDirectorVm> ProjectDirector { get; set; }
         public List<FinancialYearAllocationVm> FinancialYearAllocation { get; set; }
         public List<FiscalYearExpenseVm> FiscalYearExpense { get; set; }
         public List<FormalMeetingVm> FormalMeeting { get; set; }
-        public List<ProjectWorkVm> ProjectWorks { get; set; }
+        public List<ProjectWorkDetailsVm> ProjectWorks { get; set; }
 
 
     }
