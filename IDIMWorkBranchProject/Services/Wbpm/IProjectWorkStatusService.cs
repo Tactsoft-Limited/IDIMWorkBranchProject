55ac32ca8307df02864169db562db991e0449e29
khpolash <khpolashbd@gmail.com>
2025-03-10 11:25:31 +0600

    Implemented IProjectWorkStatusService interface and ProjectWorkStatusService class with GetByProjectWorkIdAsync method to fetch ProjectWorkStatus by ProjectWorkId

---
+++ IDIMWorkBranchProject/Services/Wbpm/IProjectWorkStatusService.cs
@@ -0,0 +1,11 @@
+﻿using BGB.Data.Entities.Wbpm;
+using IDIMWorkBranchProject.Services.Base;
+using System.Threading.Tasks;
+
+namespace IDIMWorkBranchProject.Services.Wbpm
+{
+    public interface IProjectWorkStatusService : IBaseService<ProjectWorkStatus>
+    {
+        Task<ProjectWorkStatus> GetByProjectWorkIdAsync(int id);
+    }
+}
---
+++ IDIMWorkBranchProject/Services/Wbpm/ProjectWorkStatusService.cs
@@ -0,0 +1,21 @@
+﻿using BGB.Data.Entities.Wbpm;
+using IDIMWorkBranchProject.Data.Database;
+using IDIMWorkBranchProject.Services.Base;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IDIMWorkBranchProject.Services.Wbpm
+{
+    public class ProjectWorkStatusService : BaseService<ProjectWorkStatus>, IProjectWorkStatusService
+    {
+        public ProjectWorkStatusService(IDIMDBEntities context) : base(context)
+        {
+        }
+
+        public async Task<ProjectWorkStatus> GetByProjectWorkIdAsync(int id)
+        {
+            return await _context.ProjectWorkStatuses.Where(x => x.ProjectWorkId == id).FirstOrDefaultAsync();
+        }
+    }
+}
