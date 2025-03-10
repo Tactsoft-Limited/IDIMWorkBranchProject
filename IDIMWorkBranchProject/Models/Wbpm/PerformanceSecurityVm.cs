c82c5fa40e306e5e0a664dfccd8d38bdbf91951f
khpolash <khpolashbd@gmail.com>
2025-03-10 11:22:13 +0600

    Added PerformanceSecurityVm view model for handling performance security data with file upload support

---
+++ IDIMWorkBranchProject/Models/Wbpm/PerformanceSecurityVm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace IDIMWorkBranchProject.Models.Wbpm
+{
+    public class PerformanceSecurityVm
+    {
+        public int PerformanceSecurityId { get; set; }
+
+        [DisplayName("নির্মাণ কাজ")]
+        public int ProjectWorkId { get; set; }
+
+        [DisplayName("নির্মাণ কাজের নাম (বাংলা)")]
+        public string ProjectWorkTitleB { get; set; }
+
+        [DisplayName("জমার তারিখ")]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime SubmissionDate { get; set; }
+
+        [DisplayName("ব্যাংক গ্যারান্টির শেষ তারিখ")]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime ExpiryDate { get; set; }
+
+        [DisplayName("ব্যাংক গ্যারান্টি ডকুমেন্ট")]
+        public string ScanDocument { get; set; }
+
+        [DisplayName("ব্যাংক গ্যারান্টি ডকুমেন্ট")]
+        public HttpPostedFileBase DocumentFile { get; set; }
+
+    }
+}
