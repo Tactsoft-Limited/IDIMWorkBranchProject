8caf5130476e9fccf511cca3eea06ac70d8ff226
khpolash <khpolashbd@gmail.com>
2025-03-10 11:44:43 +0600

    Refactor ProjectWorkDetailsVm: Added new properties for Noha, PerformanceSecurity, ContractAgreement, WorkOrder, and ProjectWorkStatus

--- IDIMWorkBranchProject/Models/Wbpm/ProjectWorkDetailsVm.cs
+++ IDIMWorkBranchProject/Models/Wbpm/ProjectWorkDetailsVm.cs
@@ -2,9 +2,14 @@
 
 namespace IDIMWorkBranchProject.Models.Wbpm
 {
-	public class ProjectWorkDetailsVm
+    public class ProjectWorkDetailsVm
     {
         public ProjectWorkVm ProjectWorks { get; set; }
+        public NohaVm Noha { get; set; }
+        public PerformanceSecurityVm PerformanceSecurity { get; set; }
+        public ContractAgreementVm ContractAgreement { get; set; }
+        public WorkOrderVm WorkOrder { get; set; }
+        public ProjectWorkStatusVm ProjectWorkStatus { get; set; }
         public List<ADPReceivePaymentVm> ADPReceivePayments { get; set; }
         public List<ContractorCompanyPaymentVm> ContractorCompanyPayments { get; set; }
 
