5919991223c8ae241224eb512ab7ce1f6e9f7c88
khpolash <khpolashbd@gmail.com>
2025-03-10 11:43:49 +0600

    Refactor ContractAgreementVm model with new fields and nullable properties
        Changed ConstructionFirm to an integer property (ConstructionCompanyId) to link to the construction company.
        Updated AgreementDate to be nullable (DateTime?).
        Added new nullable properties for various IDs and names related to project roles, such as AddDGId, DDGId, ProjectDirectorId, and DirectorId.
        Added new string properties for role names: AddDGName, DDGName, ProjectDirectorName, and DirectorName.
        Updated ScanDocument to store the document name as a string and added DocumentFile as an HttpPostedFileBase for file uploads.

--- IDIMWorkBranchProject/Models/Wbpm/ContractAgreementVm.cs
+++ IDIMWorkBranchProject/Models/Wbpm/ContractAgreementVm.cs
@@ -1,38 +1,51 @@
 ﻿using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Web;
 
 namespace IDIMWorkBranchProject.Models.Wbpm
 {
 
     public class ContractAgreementVm
     {
-        public ContractAgreementVm()
-        {
-    
-        }
+
         [Key]
         public int ContractAgreementId { get; set; }
 
-        [Display(Name ="নির্মান কাজ আইডি")]
+        [Display(Name = "নির্মান কাজ")]
         public int ProjectWorkId { get; set; }
 
         [DisplayName("নির্মান কাজের শিরোনাম")]
         public string ProjectWorkTitle { get; set; }
 
         [Display(Name = "ঠিকাদার প্রতিষ্ঠান")]
+        public int ConstructionCompanyId { get; set; }
+
+        [Display(Name = "ঠিকাদার প্রতিষ্ঠান")]
         public string ConstructionFirm { get; set; }
 
-        [Display(Name = "চুক্তির দিন")]
-        public string ContractDay { get; set; }
         [Display(Name = "চুক্তির তারিখ")]
-        public DateTime ContractDate { get; set; }
+        public DateTime? AgreementDate { get; set; }
 
-        [Display(Name = "চুক্তির পরিমান")]
-        public decimal? AgreementCost { get; set; }
+        public int? AddDGId { get; set; }
+        public string AddDGName { get; set; }
+        public int? DDGId { get; set; }
 
-        [Display(Name = "চুক্তির পরিমান কথায়")]
-        public string AgreementCostInWord { get; set; }
+        public string DDGName { get; set; }
 
+        public int? ProjectDirectorId { get; set; }
+
+        public string ProjectDirectorName { get; set; }
+
+        public int? DirectorId { get; set; }
+
+        public string DirectorName { get; set; }
+
+
+        [DisplayName("চুক্তি ডকুমেন্ট")]
+        public string ScanDocument { get; set; }
+
+        [DisplayName("চুক্তি ডকুমেন্ট")]
+        public HttpPostedFileBase DocumentFile { get; set; }
     }
 }
