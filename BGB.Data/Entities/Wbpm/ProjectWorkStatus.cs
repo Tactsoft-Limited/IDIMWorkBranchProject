ff46e564423471738dd7fdc6339ba88076c6961d
khpolash <khpolashbd@gmail.com>
2025-03-10 11:21:16 +0600

    Added ProjectWorkStatus class with properties and foreign key relationship to ProjectWork

---
+++ BGB.Data/Entities/Wbpm/ProjectWorkStatus.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using BGB.Data.Entities.Base;
+
+namespace BGB.Data.Entities.Wbpm
+{
+    [Table("ProjectWorkStatus", Schema = "wbpm")]
+    public class ProjectWorkStatus : BaseEntity
+    {
+        [Key]
+        public int ProjectWorkStatusId { get; set; }
+        public int ProjectWorkId { get; set; }
+        public int? StatusTypeId { get; set; }
+
+
+
+        [ForeignKey(nameof(ProjectWorkId))]
+        public virtual ProjectWork ProjectWork { get; set; }
+    }
+}
