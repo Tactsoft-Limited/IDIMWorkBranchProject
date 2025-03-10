552b52b09a4dc036d4b30d0fe5a0280aa556d724
khpolash <khpolashbd@gmail.com>
2025-03-10 11:20:09 +0600

    Added new properties and navigation relationships to ContractAgreement class, including ConstructionCompany, RecruitmentCommittee, and nullable fields

--- BGB.Data/Entities/Wbpm/ContractAgreement.cs
+++ BGB.Data/Entities/Wbpm/ContractAgreement.cs
@@ -5,19 +5,40 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BGB.Data.Entities.Wbpm
 {
-    [Table("ContractAgreement", Schema ="wbpm")]
+    [Table("ContractAgreement", Schema = "wbpm")]
     public class ContractAgreement : BaseEntity
     {
         [Key]
         public int ContractAgreementId { get; set; }
         public int ProjectWorkId { get; set; }
-        public string ContractDay { get; set; }
-        public DateTime ContractDate { get; set; }
-        public decimal AgreementCost { get; set; }
-        public string AgreementCostInWord { get; set; }
+        public int ConstructionCompanyId { get; set; }
+        public DateTime? AgreementDate { get; set; }
+        public int? AddDGId { get; set; }
+        public int? DDGId { get; set; }
+        public int? ProjectDirectorId { get; set; }
+        public int? DirectorId { get; set; }
+        public string ScanDocument { get; set; }
 
 
+
+
+        // Navigation Properties (Foreign Key Relationships)
         [ForeignKey(nameof(ProjectWorkId))]
         public ProjectWork ProjectWork { get; set; }
+
+        [ForeignKey(nameof(ConstructionCompanyId))]
+        public virtual ConstructionCompany ConstructionCompany { get; set; }
+
+        [ForeignKey(nameof(AddDGId))]
+        public virtual RecruitmentCommittee AddDG { get; set; }
+
+        [ForeignKey(nameof(DDGId))]
+        public virtual RecruitmentCommittee DDG { get; set; }
+
+        [ForeignKey(nameof(ProjectDirectorId))]
+        public virtual RecruitmentCommittee ProjectDirector { get; set; }
+
+        [ForeignKey(nameof(DirectorId))]
+        public virtual RecruitmentCommittee Director { get; set; }
     }
 }
