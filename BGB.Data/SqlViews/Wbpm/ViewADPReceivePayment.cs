071278c134610c0539750d047a965aa2dc1961b8
khpolash <khpolashbd@gmail.com>
2025-03-13 15:32:23 +0600

    Added HeadAssistant, ConcernedEngineer, SectionIC, and BranchClerk properties to ViewADPReceivePayment class.

--- BGB.Data/SqlViews/Wbpm/ViewADPReceivePayment.cs
+++ BGB.Data/SqlViews/Wbpm/ViewADPReceivePayment.cs
@@ -32,5 +32,10 @@ namespace BGB.Data.SqlViews.Wbpm
         public double? TotalReceivePer { get; set; }
         public decimal? TotalReceiveAmount { get; set; }
         public string TotalReceiveAmountInWord { get; set; }
+
+        public string HeadAssistant { get; set; }
+        public string ConcernedEngineer { get; set; }
+        public string SectionIC { get; set; }
+        public string BranchClerk { get; set; }
     }
 }
