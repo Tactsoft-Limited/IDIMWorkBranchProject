c569aad6311babb33c741c57a2b4f7d2b1507ba5
khpolash <khpolashbd@gmail.com>
2025-03-20 10:48:26 +0600

    Add GetPagedAsync method to ContractorCompanyPaymentService for paginated search and sorting

--- IDIMWorkBranchProject/Services/Wbpm/ContractorCompanyPaymentService.cs
+++ IDIMWorkBranchProject/Services/Wbpm/ContractorCompanyPaymentService.cs
@@ -1,9 +1,11 @@
 ﻿using BGB.Data.Entities.Wbpm;
 using IDIMWorkBranchProject.Data.Database;
+using IDIMWorkBranchProject.Models.Wbpm;
 using IDIMWorkBranchProject.Services.Base;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Threading.Tasks;
 
 namespace IDIMWorkBranchProject.Services.Wbpm
@@ -16,7 +18,42 @@ namespace IDIMWorkBranchProject.Services.Wbpm
 
         public async Task<List<ContractorCompanyPayment>> GetByProjectWorkIdAsync(int id)
         {
-            return await _context.ContractorCompanyPayments.Where(x=>x.ProjectWorkId==id).ToListAsync();
+            return await _context.ContractorCompanyPayments.Where(x => x.ProjectWorkId == id).ToListAsync();
+        }
+
+        public async Task<object> GetPagedAsync(ContractorCompanyPaymentSearchVm model)
+        {
+            var query = _context.ContractorCompanyPayments.Where(x =>
+            (string.IsNullOrEmpty(model.SearchValue) ||
+            x.ProjectWork.ProjectWorkTitle.Contains(model.SearchValue)));
+
+            query = !string.IsNullOrEmpty(model.SortColumn) && !string.IsNullOrEmpty(model.SortDirection)
+            ? query.OrderBy($"{model.SortColumn} {model.SortDirection}")
+                : query.OrderBy(x => x.ContractorCompanyPaymentId);  // Default ordering by SubProjectId
+
+            var totalRecords = await query.CountAsync();
+            var filteredRecords = await query.CountAsync();
+            var pagedData = await query.Skip(model.PageIndex * model.PageSize).Take(model.PageSize).ToListAsync();
+
+            // Return the response in DataTables format
+            var result = new
+            {
+                draw = model.Draw,
+                recordsTotal = totalRecords,
+                recordsFiltered = filteredRecords,
+                data = pagedData.Select(x => new ContractorCompanyPaymentVm
+                {
+                    ContractorCompanyPaymentId = x.ContractorCompanyPaymentId,
+                    ProjectWorkId = x.ProjectWorkId,
+                    ProjectWorkTitle = x.ProjectWork.ProjectWorkTitle,
+                    ProjectWorkTitleB = x.ProjectWork.ProjectWorkTitleB,
+                    LetterNo = x.LetterNo,
+                    BillPaymentNumber = x.BillPaymentNumber,
+                    FinalPaymentAmount = x.FinalPaymentAmount,
+                })
+            };
+
+            return result;
         }
     }
 }
