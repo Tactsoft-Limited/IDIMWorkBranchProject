b0c100d73cc976595160e2aa5508947862873246
Hemayet-Mondol-b16 <workmailhemayet@gmail.com>
2025-04-27 17:23:34 +0600

    Add HandoverApproved class to Wbpm namespace
    Introduces the `HandoverApproved` class inheriting from `BaseEntity`, mapped to the "HandoverApproved" table in the "wbpm" schema. The class includes properties for various identifiers and dates, along with necessary using directives.

---
+++ BGB.Data/Entities/Wbpm/HandoverApproved.cs
@@ -0,0 +1,31 @@
+﻿using BGB.Data.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGB.Data.Entities.Wbpm
+{
+    [Table("HandoverApproved", Schema = "wbpm")]
+    public class HandoverApproved:BaseEntity
+    {
+        [Key]
+        public int HandoverApprovedId { get; set; }
+        public int ProjectWorkId { get; set; }
+        public string LetterNo { get; set; }
+        public string QuoateOne { get; set; }
+        public string QuoateTwo { get; set; }
+        public DateTime? QuoateOneDate { get; set; }
+        public DateTime? QuoateTwoDate { get; set; }
+        public string Description { get; set; }
+
+        public int? HeadAssistantId { get; set; }
+        public int? ConcernedEngineerId { get; set; }
+        public int? SectionICId { get; set; }
+        public int? BranchClerkId { get; set; }
+
+    }
+}
