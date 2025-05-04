ebdd32167caa6dfa10ba03aef66130047a2f850d
Hemayet-Mondol-b16 <workmailhemayet@gmail.com>
2025-05-04 12:41:10 +0600

    Add revenue services and interfaces for project work
    Introduces `IRevenueNohaService` and `IRevenuePerformanceSecurityService` interfaces with asynchronous methods to retrieve entities by project work ID. Implements `RevenueNohaService` and `RevenuePerformanceSecurityService` classes that inherit from `BaseService` and utilize Entity Framework for database queries. Adds necessary namespaces to support the new functionality.

---
+++ IDIMWorkBranchProject/Services/Wbpm/IRevenueNohaService.cs
@@ -0,0 +1,15 @@
+﻿
+
+using BGB.Data.Entities.Wbpm;
+using IDIMWorkBranchProject.Services.Base;
+using System.Threading.Tasks;
+
+namespace IDIMWorkBranchProject.Services.Wbpm
+{
+    public interface IRevenueNohaService:IBaseService<RevenueNoha>
+    {
+        Task<RevenueNoha> GetByProjectWorkIdAsync(int id);
+
+
+    }
+}
