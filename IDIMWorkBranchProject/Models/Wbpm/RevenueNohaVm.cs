c95706351996a1a03a378f9804e59608196269c5
Hemayet-Mondol-b16 <workmailhemayet@gmail.com>
2025-05-04 12:40:58 +0600

    Add view models for revenue management
    Introduced `RevenueNohaVm` and `RevenuePerformanceSecurityVm` classes in the `IDIMWorkBranchProject.Models.Wbpm` namespace. These classes encapsulate properties for managing revenue-related data, including project work IDs, document details, and relevant dates, with appropriate data annotations for display and formatting.

---
+++ IDIMWorkBranchProject/Models/Wbpm/RevenueNohaVm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace IDIMWorkBranchProject.Models.Wbpm
+{
+    public class RevenueNohaVm
+    {
+        public int RevenueNohaId { get; set; }
+
+        [DisplayName("নির্মাণ কাজ")]
+        public int ProjectWorkId { get; set; }
+
+        [DisplayName("নির্মাণ কাজের নাম (বাংলা)")]
+        public string ProjectWorkTitleB { get; set; }
+
+        [DisplayName("লেটার নং")]
+        public string LetterNo { get; set; }
+
+        [DisplayName(" রাজস্ব এনওএইচ তারিখ")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? RevenueNohaDate { get; set; }
+
+        [DisplayName("রাজস্ব এনওএইচএ ডকুমেন্ট")]
+        public string ScanDocument { get; set; }
+
+        [DisplayName("রাজস্ব এনওএইচএ ডকুমেন্ট")]
+        public HttpPostedFileBase DocumentFile { get; set; }
+    }
+}
---
+++ IDIMWorkBranchProject/Models/Wbpm/RevenuePerformanceSecurityVm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Web;
+
+namespace IDIMWorkBranchProject.Models.Wbpm
+{
+    public class RevenuePerformanceSecurityVm
+    {
+        public int RevenuePerformanceSecurityId { get; set; }
+
+        [DisplayName("নির্মাণ কাজ")]
+        public int ProjectWorkId { get; set; }
+
+        [DisplayName("নির্মাণ কাজের নাম (বাংলা)")]
+        public string ProjectWorkTitleB { get; set; }
+
+        [DisplayName("জমার তারিখ")]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime RevenuePerformanceSecuritySubmissionDate { get; set; }
+
+        [DisplayName("ব্যাংক গ্যারান্টির শেষ তারিখ")]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime RevenuePerformanceSecurityExpiryDate { get; set; }
+
+        [DisplayName("ব্যাংক গ্যারান্টি ডকুমেন্ট")]
+        public string RevenuePerformanceSecurityScanDocument { get; set; }
+
+        [DisplayName("ব্যাংক গ্যারান্টি ডকুমেন্ট")]
+        public HttpPostedFileBase DocumentFile { get; set; }
+    }
+}
