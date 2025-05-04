5e3ff62142a2325cbe6ee17ca6155fb3b2c59ab0
Hemayet-Mondol-b16 <workmailhemayet@gmail.com>
2025-05-04 12:40:29 +0600

    Add RevenueNoha and RevenuePerformanceSecurity controllers
    This commit introduces two new controllers: `RevenueNohaController` and `RevenuePerformanceSecurityController` in the `IDIMWorkBranchProject.Controllers.Wbpm` namespace. Both controllers inherit from `BaseController` and provide methods for creating revenue-related entities. The `RevenueNohaController` manages revenue noha files, while the `RevenuePerformanceSecurityController` handles revenue performance security files. Each controller initializes necessary services and includes asynchronous methods for record creation, file uploads, and error handling. File storage paths are defined, and required using directives are added.

---
+++ IDIMWorkBranchProject/Controllers/Wbpm/RevenueNohaController.cs
@@ -0,0 +1,116 @@
+﻿using AutoMapper;
+using BGB.Data.Entities.Wbpm;
+using IDIMWorkBranchProject.Extentions;
+using IDIMWorkBranchProject.Models.Wbpm;
+using IDIMWorkBranchProject.Services;
+using IDIMWorkBranchProject.Services.Wbpm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IDIMWorkBranchProject.Controllers.Wbpm
+{
+    public class RevenueNohaController : BaseController
+    {
+        private readonly IProjectWorkService _projectWorkService;
+        private readonly IRevenueNohaService _revenueNohaService;
+        private readonly IMapper _mapper;
+        private readonly string fileStorePath = "Documents/RevenuneNohaFiles";
+        public RevenueNohaController(IActivityLogService activityLogService, IRevenueNohaService revenueNohaService, IMapper mapper, IProjectWorkService projectWorkService) : base(activityLogService)
+        {
+            _revenueNohaService = revenueNohaService;
+            _mapper = mapper;
+            _projectWorkService = projectWorkService;
+        }
+
+        // GET: RevenueNoha
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Create(int id)
+        {
+            var projectWork = await _projectWorkService.GetByIdAsync(id);
+            var revenueNoha = await _revenueNohaService.GetByProjectWorkIdAsync(id);
+
+            var model = new RevenueNohaVm
+            {
+                ProjectWorkId = projectWork.ProjectWorkId,
+                ProjectWorkTitleB = projectWork.ProjectWorkTitleB,
+            };
+            if (revenueNoha != null)
+            {
+                model.RevenueNohaId = revenueNoha.RevenueNohaId;
+                model.RevenueNohaDate = revenueNoha.RevenueNohaDate;
+                model.LetterNo = revenueNoha.LetterNo;
+                model.ScanDocument = revenueNoha.ScanDocument;
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(RevenueNohaVm model)
+        {
+            var projectWork = await _projectWorkService.GetByIdAsync(model.ProjectWorkId);
+            try
+            {
+                string fileName = null;
+                if (model.RevenueNohaId > 0)
+                {
+                    if (model.DocumentFile != null && model.DocumentFile.ContentLength > 0)
+                    {
+                        //delete existing file
+                        FileExtention.DeleteFile(model.ScanDocument, fileStorePath);
+
+                        fileName = FileExtention.UploadFile(model.DocumentFile, fileStorePath);
+                        if (fileName != null)
+                        {
+                            model.ScanDocument = fileName;
+                        }
+                        else
+                        {
+                            TempData["Message"] = Messages.FileUploadFailed(MessageType.Create.ToString());
+                            return View(model);
+                        }
+                    }
+                    await _revenueNohaService.UpdateAsync(_mapper.Map<RevenueNoha>(model));
+                    TempData["Message"] = Messages.Success(MessageType.Update.ToString());
+                }
+                else
+                {
+                    if (model.DocumentFile != null && model.DocumentFile.ContentLength > 0)
+                    {
+                        fileName = FileExtention.UploadFile(model.DocumentFile, fileStorePath);
+                        if (fileName != null)
+                        {
+                            model.ScanDocument = fileName;
+                        }
+                        else
+                        {
+                            TempData["Message"] = Messages.FileUploadFailed(MessageType.Create.ToString());
+                            return View(model);
+                        }
+                        await _revenueNohaService.CreateAsync(_mapper.Map<RevenueNoha>(model));
+                        ////projectWork.IsNoahCompleted = true;
+                        //await _projectWorkService.UpdateAsync(projectWork);
+
+                        TempData["Message"] = Messages.Success(MessageType.Create.ToString());
+                    }
+                }
+                return RedirectToAction(nameof(ProjectWorkController.Details), nameof(ProjectWork), new { id = model.ProjectWorkId });
+            }
+            catch (Exception exception)
+            {
+                TempData["Message"] = Messages.Failed(MessageType.Create.ToString(), $"An error occurred while processing your request.{exception.InnerException.Message}");
+                return View(model);
+            }
+
+        }
+
+    }
+}
---
+++ IDIMWorkBranchProject/Controllers/Wbpm/RevenuePerformanceSecurityController.cs
@@ -0,0 +1,109 @@
+﻿using AutoMapper;
+using BGB.Data.Entities.Wbpm;
+using IDIMWorkBranchProject.Extentions;
+using IDIMWorkBranchProject.Models.Wbpm;
+using IDIMWorkBranchProject.Services;
+using IDIMWorkBranchProject.Services.Wbpm;
+using System.Threading.Tasks;
+using System;
+using System.Web.Mvc;
+
+namespace IDIMWorkBranchProject.Controllers.Wbpm
+{
+    public class RevenuePerformanceSecurityController : BaseController
+    {
+        private readonly IRevenuePerformanceSecurityService _revenuePerformanceSecurityService;
+        private readonly IMapper _mapper;
+        private readonly IProjectWorkService _projectWorkService;
+        private readonly string fileStorePath = "Documents/RevenuePerformanceSecurityFiles";
+        public RevenuePerformanceSecurityController(IActivityLogService activityLogService, IRevenuePerformanceSecurityService revenuePerformanceSecurityService, IMapper mapper, IProjectWorkService projectWorkService) : base(activityLogService)
+        {
+            _revenuePerformanceSecurityService = revenuePerformanceSecurityService;
+            _mapper = mapper;
+            _projectWorkService = projectWorkService;
+        }
+
+        // GET: RevenuePerformanceSecurity
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public async Task<ActionResult> Create(int id)
+        {
+            var projectWork = await _projectWorkService.GetByIdAsync(id);
+            var revenuePerformanceSecurity = await _revenuePerformanceSecurityService.GetByProjectWorkIdAsync(id);
+
+            var model = new RevenuePerformanceSecurityVm
+            {
+                ProjectWorkId = projectWork.ProjectWorkId,
+                ProjectWorkTitleB = projectWork.ProjectWorkTitleB,
+            };
+            if (revenuePerformanceSecurity != null)
+            {
+                model.RevenuePerformanceSecurityId = revenuePerformanceSecurity.RevenuePerformanceSecurityId;
+                model.RevenuePerformanceSecuritySubmissionDate = revenuePerformanceSecurity.RevenuePerformanceSecuritySubmissionDate;
+                model.RevenuePerformanceSecurityExpiryDate = revenuePerformanceSecurity.RevenuePerformanceSecurityExpiryDate;
+                model.RevenuePerformanceSecurityScanDocument = revenuePerformanceSecurity.RevenuePerformanceSecurityScanDocument;
+            }
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(RevenuePerformanceSecurityVm model)
+        {
+            var projectWork = await _projectWorkService.GetByIdAsync(model.ProjectWorkId);
+            string fileName = null;
+            try
+            {
+                if (model.RevenuePerformanceSecurityId > 0)
+                {
+                    if (model.DocumentFile != null && model.DocumentFile.ContentLength > 0)
+                    {
+                        //delete existing file
+                        FileExtention.DeleteFile(model.RevenuePerformanceSecurityScanDocument, fileStorePath);
+
+                        fileName = FileExtention.UploadFile(model.DocumentFile, fileStorePath);
+                        if (fileName != null)
+                        {
+                            model.RevenuePerformanceSecurityScanDocument = fileName;
+                        }
+                        else
+                        {
+                            TempData["Message"] = Messages.FileUploadFailed(MessageType.Create.ToString());
+                            return View(model);
+                        }
+                    }
+                    await _revenuePerformanceSecurityService.UpdateAsync(_mapper.Map<RevenuePerformanceSecurity>(model));
+                    TempData["Message"] = Messages.Success(MessageType.Update.ToString());
+                }
+                else
+                {
+                    if (model.DocumentFile != null && model.DocumentFile.ContentLength > 0)
+                    {
+                        fileName = FileExtention.UploadFile(model.DocumentFile, fileStorePath);
+                        if (fileName != null)
+                        {
+                            model.RevenuePerformanceSecurityScanDocument = fileName;
+                        }
+                        else
+                        {
+                            TempData["Message"] = Messages.FileUploadFailed(MessageType.Create.ToString());
+                            return View(model);
+                        }
+                        await _revenuePerformanceSecurityService.CreateAsync(_mapper.Map<RevenuePerformanceSecurity>(model));
+                        //projectWork.IsPerformanceSecuritySubmited = true;
+                        //await _projectWorkService.UpdateAsync(projectWork);
+                        TempData["Message"] = Messages.Success(MessageType.Create.ToString());
+                    }
+                }
+                return RedirectToAction("details/" + model.ProjectWorkId, "ProjectWork");
+            }
+            catch (Exception exception)
+            {
+                TempData["Message"] = Messages.Failed(MessageType.Create.ToString(), $"An error occurred while processing your request.{exception.InnerException.Message}");
+                return View(model);
+            }
+
+        }
+    }
+}
