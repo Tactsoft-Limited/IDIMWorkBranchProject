5e37a06c4c2f312b0d4439c679a6dd069029e0ad
Hemayet-Mondol-b16 <workmailhemayet@gmail.com>
2025-05-07 12:01:51 +0600

    Refactor RevenueNohaController to use IRevenueService
    Updated RevenueNohaController to replace _projectWorkService with _revenueService. Adjusted constructor and Create methods to fetch revenue data. Modified document handling and changed return behavior of Create method to return the view with the model.

--- IDIMWorkBranchProject/Controllers/Wbpm/RevenueNohaController.cs
+++ IDIMWorkBranchProject/Controllers/Wbpm/RevenueNohaController.cs
@@ -15,15 +15,15 @@ namespace IDIMWorkBranchProject.Controllers.Wbpm
 {
     public class RevenueNohaController : BaseController
     {
-        private readonly IProjectWorkService _projectWorkService;
         private readonly IRevenueNohaService _revenueNohaService;
+        private readonly IRevenueService _revenueService;
         private readonly IMapper _mapper;
         private readonly string fileStorePath = "Documents/RevenuneNohaFiles";
-        public RevenueNohaController(IActivityLogService activityLogService, IRevenueNohaService revenueNohaService, IMapper mapper, IProjectWorkService projectWorkService) : base(activityLogService)
+        public RevenueNohaController(IActivityLogService activityLogService, IRevenueNohaService revenueNohaService, IMapper mapper, IProjectWorkService projectWorkService, IRevenueService revenueService) : base(activityLogService)
         {
             _revenueNohaService = revenueNohaService;
             _mapper = mapper;
-            _projectWorkService = projectWorkService;
+            _revenueService = revenueService;
         }
 
         // GET: RevenueNoha
@@ -34,20 +34,20 @@ namespace IDIMWorkBranchProject.Controllers.Wbpm
 
         public async Task<ActionResult> Create(int id)
         {
-            var projectWork = await _projectWorkService.GetByIdAsync(id);
-            var revenueNoha = await _revenueNohaService.GetByProjectWorkIdAsync(id);
+            var revenue = await _revenueService.GetByIdAsync(id);
+            var revenueNoha = await _revenueNohaService.GetByRevenueIdAsync(id);
 
             var model = new RevenueNohaVm
             {
-                ProjectWorkId = projectWork.ProjectWorkId,
-                ProjectWorkTitleB = projectWork.ProjectWorkTitleB,
+                RevenueId = revenue.RevenueId,
+                RevenueWorkTitleB = revenue.WorkTitleB,
             };
             if (revenueNoha != null)
             {
                 model.RevenueNohaId = revenueNoha.RevenueNohaId;
                 model.RevenueNohaDate = revenueNoha.RevenueNohaDate;
-                model.LetterNo = revenueNoha.LetterNo;
-                model.ScanDocument = revenueNoha.ScanDocument;
+                model.LetterNo = revenueNoha.LetterNo;                
+                model.RevenueNohaScanDocument = revenueNoha.RevenueNohaScanDocument;                
             }
             return View(model);
         }
@@ -56,21 +56,21 @@ namespace IDIMWorkBranchProject.Controllers.Wbpm
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create(RevenueNohaVm model)
         {
-            var projectWork = await _projectWorkService.GetByIdAsync(model.ProjectWorkId);
+            var revenue = await _revenueService.GetByIdAsync(model.RevenueId);
             try
             {
                 string fileName = null;
                 if (model.RevenueNohaId > 0)
-                {
+                {                    
                     if (model.DocumentFile != null && model.DocumentFile.ContentLength > 0)
                     {
                         //delete existing file
-                        FileExtention.DeleteFile(model.ScanDocument, fileStorePath);
+                        FileExtention.DeleteFile(model.RevenueNohaScanDocument, fileStorePath);
 
                         fileName = FileExtention.UploadFile(model.DocumentFile, fileStorePath);
                         if (fileName != null)
                         {
-                            model.ScanDocument = fileName;
+                            model.RevenueNohaScanDocument = fileName;
                         }
                         else
                         {
@@ -78,9 +78,9 @@ namespace IDIMWorkBranchProject.Controllers.Wbpm
                             return View(model);
                         }
                     }
-                    await _revenueNohaService.UpdateAsync(_mapper.Map<RevenueNoha>(model));
                     TempData["Message"] = Messages.Success(MessageType.Update.ToString());
                 }
+
                 else
                 {
                     if (model.DocumentFile != null && model.DocumentFile.ContentLength > 0)
@@ -88,7 +88,7 @@ namespace IDIMWorkBranchProject.Controllers.Wbpm
                         fileName = FileExtention.UploadFile(model.DocumentFile, fileStorePath);
                         if (fileName != null)
                         {
-                            model.ScanDocument = fileName;
+                            model.RevenueNohaScanDocument = fileName;
                         }
                         else
                         {
@@ -96,13 +96,13 @@ namespace IDIMWorkBranchProject.Controllers.Wbpm
                             return View(model);
                         }
                         await _revenueNohaService.CreateAsync(_mapper.Map<RevenueNoha>(model));
-                        ////projectWork.IsNoahCompleted = true;
-                        //await _projectWorkService.UpdateAsync(projectWork);
+                        revenue.IsNoahCompleted = true;
+                        await _revenueService.UpdateAsync(revenue);
 
                         TempData["Message"] = Messages.Success(MessageType.Create.ToString());
                     }
                 }
-                return RedirectToAction(nameof(ProjectWorkController.Details), nameof(ProjectWork), new { id = model.ProjectWorkId });
+                return View(model);
             }
             catch (Exception exception)
             {
