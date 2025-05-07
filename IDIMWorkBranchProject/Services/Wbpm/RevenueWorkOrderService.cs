a3d4c5fcb1039076fc30cdd745a1204f14860b72
Hemayet-Mondol-b16 <workmailhemayet@gmail.com>
2025-05-07 12:04:06 +0600

    Add RevenueWorkOrder service and interface
    Introduced a new interface `IRevenueWorkOrderService` that extends `IBaseService<RevenueWorkOrder>` in `IRevenueWorkOrderService.cs`.
    
    Implemented the `RevenueWorkOrderService` class in `RevenueWorkOrderService.cs`, inheriting from `BaseService<RevenueWorkOrder>` and implementing the new interface. Added necessary using directives in both files.

---
+++ IDIMWorkBranchProject/Services/Wbpm/RevenueWorkOrderService.cs
@@ -0,0 +1,17 @@
+﻿using BGB.Data.Entities.Wbpm;
+using IDIMWorkBranchProject.Data.Database;
+using IDIMWorkBranchProject.Services.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IDIMWorkBranchProject.Services.Wbpm
+{
+    public class RevenueWorkOrderService : BaseService<RevenueWorkOrder>, IRevenueWorkOrderService
+    {
+        public RevenueWorkOrderService(IDIMDBEntities context) : base(context)
+        {
+        }
+    }
+}
