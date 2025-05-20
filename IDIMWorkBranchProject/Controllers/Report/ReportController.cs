d0b2b4b8f8497eaa9e8b68a9c79bcb8c0845753d
Hemayet-Mondol-b16 <workmailhemayet@gmail.com>
2025-05-20 16:40:02 +0600

    Add PrintRevenueContractAgreement method to ReportController
    Introduces a new asynchronous method `PrintRevenueContractAgreement` in the `ReportController` class. This method generates a revenue report based on the provided `id`, retrieves data using `_reportService.GetRevenueReportAsync(id)`, and constructs a `ReportDataSource` and `ReportConfig`. It returns a `ReportResult` upon success and handles exceptions by throwing an `InvalidOperationException` with a descriptive message.

--- IDIMWorkBranchProject/Controllers/Report/ReportController.cs
+++ IDIMWorkBranchProject/Controllers/Report/ReportController.cs
@@ -266,5 +266,34 @@ namespace IDIMWorkBranchProject.Controllers.Report
             }
 
         }
+
+        public async Task<ActionResult> PrintRevenueContractAgreement(int id)
+        {
+
+            try
+            {
+                var data = await _reportService.GetRevenueReportAsync(id);
+
+                var reportDataSource = new List<ReportDataSource>
+                {
+                    new ReportDataSource("DsRevenueContractAgreement", data)
+
+                };
+
+                var config = new ReportConfig
+                {
+                    ReportFilePath = Path.Combine(Server.MapPath("~/Report/rdlc/revenue"), "RevenueContractAgreementReport.rdlc"),
+                };
+
+                return new ReportResult(config, reportDataSource);
+            }
+            catch (Exception exception)
+            {
+                // Log the exception if necessary
+                // You can also throw a custom exception if you want
+                throw new InvalidOperationException("An error occurred while generating the report.", exception);
+            }
+
+        }
     }
 }
