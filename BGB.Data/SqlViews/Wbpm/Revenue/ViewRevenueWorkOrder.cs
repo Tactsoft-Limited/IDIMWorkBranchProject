9a20bc350cbd31b0c9ada5aa402accf9f9cb8f7c
Hemayet-Mondol-b16 <workmailhemayet@gmail.com>
2025-06-25 14:46:49 +0600

    Add ViewRevenueWorkOrder class for revenue tracking
    Introduces the `ViewRevenueWorkOrder` class in the
    `BGB.Data.SqlViews.Wbpm.Revenue` namespace. This class
    is mapped to a database table and includes properties
    for various attributes related to revenue work orders,
    such as `RevenueWorkOrderId`, `RevenueId`, and
    `RevenueWorkOrderDate`, with appropriate data types
    including nullable options.

---
+++ BGB.Data/SqlViews/Wbpm/Revenue/ViewRevenueWorkOrder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGB.Data.SqlViews.Wbpm.Revenue
+{
+    [Table("ViewRevenueWorkOrder", Schema = "wbpm")]
+    public class ViewRevenueWorkOrder
+    {
+        [Key]
+        [Column(Order = 0)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int RevenueWorkOrderId { get; set; }
+        public int RevenueId { get; set; }
+        public string RevenueWorkOrderLetterNo { get; set; }
+        public DateTime? RevenueWorkOrderDate { get; set; }
+        public DateTime? RevenueWorkOrderStartDate { get; set; }
+        public DateTime? RevenueWorkOrderEndDate { get; set; }
+        public string WorkTitleB { get; set; }
+        public decimal? EstimateCost { get; set; }
+        public string EstimateCostInWordB { get; set; }
+        public DateTime? RevenueNohaDate { get; set; }
+        public DateTime? AgreementDate { get; set; }
+        public string FirmNameB { get; set; }
+        public string ContactPersonB { get; set; }
+        public string FirmAddressB { get; set; }
+    }
+}
