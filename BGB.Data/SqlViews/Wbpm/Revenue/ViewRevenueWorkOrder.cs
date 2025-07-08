31c15f5f48020f5c8ebcc5255a775a1a495cb2c5
Hemayet-Mondol-b16 <workmailhemayet@gmail.com>
2025-07-08 12:15:43 +0600

    Refactor revenue class structure and update properties
    Updated `BGB.Data.csproj` to move revenue-related classes
    from `Entities\Wbpm\Revenue\` to `Entities\Wbpm\Revenues\`.
    Modified properties in `ViewRevenueWorkOrder` class,
    changing some to non-nullable types and adding new properties
    for improved data handling.

--- BGB.Data/SqlViews/Wbpm/Revenue/ViewRevenueWorkOrder.cs
+++ BGB.Data/SqlViews/Wbpm/Revenue/ViewRevenueWorkOrder.cs
@@ -14,19 +14,23 @@ namespace BGB.Data.SqlViews.Wbpm.Revenue
         [Key]
         [Column(Order = 0)]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
-        public int RevenueWorkOrderId { get; set; }
         public int RevenueId { get; set; }
-        public string RevenueWorkOrderLetterNo { get; set; }
         public DateTime? RevenueWorkOrderDate { get; set; }
         public DateTime? RevenueWorkOrderStartDate { get; set; }
         public DateTime? RevenueWorkOrderEndDate { get; set; }
+
+        public string RevenueWorkOrderLetterNo { get; set; }
+
         public string WorkTitleB { get; set; }
-        public decimal? EstimateCost { get; set; }
+        public decimal EstimateCost { get; set; }
         public string EstimateCostInWordB { get; set; }
+
+        public string FirmNameB { get; set; }
+        public string OwnerDesignationB { get; set; }
+        public string OwnerNameB { get; set; }
+        public string FirmAddressB { get; set; }
+
         public DateTime? RevenueNohaDate { get; set; }
         public DateTime? AgreementDate { get; set; }
-        public string FirmNameB { get; set; }
-        public string ContactPersonB { get; set; }
-        public string FirmAddressB { get; set; }
     }
 }
